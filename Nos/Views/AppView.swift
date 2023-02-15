475f41a3f956db613891653ab8e09a49046518ac
Jason Cheatham <jc.swift.23@icloud.com>
2023-02-15 15:28:02 -0600

    Added comment

--- Nos/Views/AppView.swift
+++ Nos/Views/AppView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// Used in the NavigationStack and added as an environmentObject so that it can be used for multiple views
 class Router: ObservableObject {
     @Published var path = NavigationPath()
 }
