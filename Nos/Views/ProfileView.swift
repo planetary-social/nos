db201aabe55ece40283a8bc13cd6d089352c9b68
Jason Cheatham <jc.swift.23@icloud.com>
2023-03-06 10:37:54 -0600

    Updated to fix the bug where if the last 10 events are .contactList we may never get any metadata

--- Nos/Views/ProfileView.swift
+++ Nos/Views/ProfileView.swift
@@ -40,9 +40,13 @@ struct ProfileView: View {
             let textSub = relayService.requestEventsFromAll(filter: textFilter)
             subscriptionIds.append(textSub)
             
-            let metaFilter = Filter(authorKeys: authors, kinds: [.metaData, .contactList], limit: 10)
+            let metaFilter = Filter(authorKeys: authors, kinds: [.metaData], limit: 1)
             let metaSub = relayService.requestEventsFromAll(filter: metaFilter)
             subscriptionIds.append(metaSub)
+            
+            let contactFilter = Filter(authorKeys: authors, kinds: [.contactList], limit: 1)
+            let contactSub = relayService.requestEventsFromAll(filter: contactFilter)
+            subscriptionIds.append(contactSub)
         }
     }
     
