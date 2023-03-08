2e0f8f6ab4983e11429c087f3997d40ea5c91204
Matthew Lorentz <matt@lorentz.is>
2023-03-08 09:44:36 -0500

    PR cleanup

--- Nos/Views/Onboarding/OnboardingView.swift
+++ Nos/Views/Onboarding/OnboardingView.swift
@@ -8,27 +8,6 @@
 import SwiftUI
 import Dependencies
 
-struct ViewDidLoadModifier: ViewModifier {
-    @State private var viewDidLoad = false
-    let action: (() -> Void)?
-    
-    func body(content: Content) -> some View {
-        content
-            .onAppear {
-                if viewDidLoad == false {
-                    viewDidLoad = true
-                    action?()
-                }
-            }
-    }
-}
-
-extension View {
-    func onViewDidLoad(perform action: (() -> Void)? = nil) -> some View {
-        self.modifier(ViewDidLoadModifier(action: action))
-    }
-}
-
 struct OnboardingView: View {
     enum OnboardingStep {
         case getStarted
