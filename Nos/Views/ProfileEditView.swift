62b6e718743f5cfb0ffeef755d940e1801b82239
Chris <chris@cascadiagames.com>
2023-03-09 14:58:11 -0600

    ProfileEditView stub

---
+++ Nos/Views/ProfileEditView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileEditView.swift
+//  Nos
+//
+//  Created by Christopher Jorgensen on 3/9/23.
+//
+
+import SwiftUI
+
+struct ProfileEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditView()
+    }
+}
