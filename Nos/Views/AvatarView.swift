cc57b81c7c3e32fef3a23c185fa64fd5ce97ea16
Matthew Lorentz <matt@lorentz.is>
2023-03-10 16:31:41 -0500

    Fix infinite spinner avatars when images fail to load (usually because they are http not https)

--- Nos/Views/AvatarView.swift
+++ Nos/Views/AvatarView.swift
@@ -15,19 +15,22 @@ struct AvatarView: View {
     
     var body: some View {
         Group {
-            if let imageUrl = imageUrl {
-                CachedAsyncImage(
-                    url: imageUrl,
-                    content: { image in
-                        image.resizable()
-                    }, placeholder: {
+            let emptyAvatar = Image.emptyAvatar
+                .resizable()
+                .renderingMode(.original)
+            
+            if let imageURL = imageUrl {
+                CachedAsyncImage(url: imageURL) { phase in
+                    if let image = phase.image {
+                        image
+                    } else if phase.error != nil {
+                        emptyAvatar
+                    } else {
                         ProgressView()
                     }
-                )
+                }
             } else {
-                Image.emptyAvatar
-                    .resizable()
-                    .renderingMode(.original)
+                emptyAvatar
             }
         }
         .frame(width: size, height: size)
