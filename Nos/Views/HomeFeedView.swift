6c61e39defba0c7b24fbf5d6dd55c167a07af95f
Matthew Lorentz <matt@lorentz.is>
2023-04-25 11:27:42 -0400

    Fix an issue where more than `subscriptionLimit` subscriptions could be opened in RelayService.

--- Nos/Views/HomeFeedView.swift
+++ Nos/Views/HomeFeedView.swift
@@ -17,6 +17,7 @@ struct HomeFeedView: View {
     @EnvironmentObject var router: Router
     @EnvironmentObject var currentUser: CurrentUser
     @Dependency(\.analytics) private var analytics
+    @AppStorage("lastHomeFeedRequestDate") var lastRequestDateUnix: TimeInterval?
     
     @FetchRequest var events: FetchedResults<Event>
     @State private var date = Date.now
@@ -30,7 +31,7 @@ struct HomeFeedView: View {
     
     init(user: Author) {
         self.user = user
-        self._events = FetchRequest(fetchRequest: Event.homeFeed(for: user, after: Date.now))
+        self._events = FetchRequest(fetchRequest: Event.homeFeed(for: user, before: Date.now))
     }
     
     func subscribeToNewEvents() async {
@@ -42,15 +43,26 @@ struct HomeFeedView: View {
             return
         }
         
-        let since = events.first(where: { $0.author?.hexadecimalPublicKey == currentUserKey })?.createdAt
+        var fetchSinceDate: Date?
+        /// Make sure the lastRequestDate was more than a minute ago
+        /// to make sure we got all the events from it.
+        if let lastRequestDateUnix {
+            let lastRequestDate = Date(timeIntervalSince1970: lastRequestDateUnix)
+            if lastRequestDate.distance(to: .now) > 60 {
+                fetchSinceDate = lastRequestDate
+                self.lastRequestDateUnix = Date.now.timeIntervalSince1970
+            }
+        } else {
+            self.lastRequestDateUnix = Date.now.timeIntervalSince1970
+        }
                 
         if !followedKeys.isEmpty {
             // TODO: we could miss events with this since filter
             let textFilter = Filter(
                 authorKeys: followedKeys, 
                 kinds: [.text, .delete, .repost], 
-                limit: 100, 
-                since: since
+                limit: 400, 
+                since: fetchSinceDate
             )
             let textSub = await relayService.openSubscription(with: textFilter)
             subscriptionIDs.append(textSub)
@@ -60,7 +72,7 @@ struct HomeFeedView: View {
         let userLikesFilter = Filter(
             authorKeys: currentUserAuthorKeys,
             kinds: [.like, .delete],
-            since: since
+            since: fetchSinceDate
         )
         let userLikesSub = await relayService.openSubscription(with: userLikesFilter)
         subscriptionIDs.append(userLikesSub)
@@ -119,7 +131,8 @@ struct HomeFeedView: View {
             date = .now
         }
         .onChange(of: date) { newDate in
-            events.nsPredicate = Event.homeFeedPredicate(for: user, after: newDate)
+            events.nsPredicate = Event.homeFeedPredicate(for: user, before: newDate)
+            Task { await subscribeToNewEvents() }
         }
         .onAppear { isVisible = true }
         .onDisappear { isVisible = false }
