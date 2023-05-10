16f81bf82b462009d1632d78fb1190ecddb2d8c3
Matthew Lorentz <matt@lorentz.is>
2023-05-10 10:32:34 -0400

    Add back old secondaryText color

--- Nos/Extensions/Assets+Planetary.swift
+++ Nos/Extensions/Assets+Planetary.swift
@@ -77,6 +77,7 @@ extension Color {
        static let profileBgTop = Color("profile-bg-top", bundle: Bundle.current)
        static let profileShadow = Color("profile-shadow", bundle: Bundle.current)
        static let relationshipViewBg = Color("relationship-view-bg", bundle: Bundle.current)
+       static let secondaryTxt = Color("secondary-txt", bundle: Bundle.current)
        static let selectedtabBg = Color("selectedtab-bg", bundle: Bundle.current)
        static let tabBgBottom = Color("tab-bg-bottom", bundle: Bundle.current)
        static let tabBgTop = Color("tab-bg-top", bundle: Bundle.current)
