ebb7fdd137d98753b40e538b9a8dc64ac83c41b2
Martin Dutra <martindsq@hotmail.com>
2023-06-05 13:28:58 -0300

    add unit test

--- NosTests/NoteParserTests.swift
+++ NosTests/NoteParserTests.swift
@@ -240,6 +240,24 @@ final class NoteNoteParserTests: XCTestCase {
         XCTAssertEqual(links[safe: 0]?.key, "\(note)")
         XCTAssertEqual(links[safe: 0]?.value, URL(string: "%\(hex)"))
     }
+
+    func testContentWithUntaggedNIP27NoteAndTaggedNIP27Profile() throws {
+        let profileDisplayName = "@npub1pu3vq..."
+        let profile = "npub1pu3vqm4vzqpxsnhuc684dp2qaq6z69sf65yte4p39spcucv5lzmqswtfch"
+        let note = "note1h2mmqfjqle48j8ytmdar22v42g5y9n942aumyxatgtxpqj29pjjsjecraw"
+        let content = "Check this nostr:\(note) from nostr:\(profile)"
+        let profileHex = "0f22c06eac1002684efcc68f568540e8342d1609d508bcd4312c038e6194f8b6"
+        let noteHex = "bab7b02640fe6a791c8bdb7a352995522842ccb55779b21bab42cc1049450ca5"
+        let tags: [[String]] = [["p", profileHex]]
+        let context = try XCTUnwrap(context)
+        let attributedContent = NoteParser.parse(content: content, tags: tags, context: context)
+        let links = attributedContent.links
+        XCTAssertEqual(links.count, 2)
+        XCTAssertEqual(links[safe: 0]?.key, "\(note)")
+        XCTAssertEqual(links[safe: 0]?.value, URL(string: "%\(noteHex)"))
+        XCTAssertEqual(links[safe: 1]?.key, "\(profileDisplayName)")
+        XCTAssertEqual(links[safe: 1]?.value, URL(string: "@\(profileHex)"))
+    }
     
     func testContentWithUntaggedProfile() throws {
         let profile = "nprofile1qqszclxx9f5haga8sfjjrulaxncvkfekj097t6f3pu65f86rvg49ehqj6f9dh"
