05e1424a010831198131ed255a50ca5460ec6a1c
boreq <boreq@0x46.net>
2023-06-22 14:53:19 +0200

    Get APNs token

--- Nos/AppDelegate.swift
+++ Nos/AppDelegate.swift
@@ -4,7 +4,15 @@ import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print("app delegate")
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("apns registered", deviceToken.base64EncodedString())
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("apns error", error)
     }
 }
