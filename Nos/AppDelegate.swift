535d1c68463bf5876fc27402cbdc156c9f4455ad
boreq <boreq@0x46.net>
2023-06-22 14:35:29 +0200

    Add AppDelegate and push notification capability

---
+++ Nos/AppDelegate.swift
@@ -0,0 +1,10 @@
+import Foundation
+import SwiftUI
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("app delegate")
+        return true
+    }
+}
