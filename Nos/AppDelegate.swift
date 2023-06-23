2c42b12c50e1d778a3431f67d317656922e0e5f4
boreq <boreq@0x46.net>
2023-06-23 13:13:18 +0200

    I found the public key

--- Nos/AppDelegate.swift
+++ Nos/AppDelegate.swift
@@ -3,13 +3,13 @@ import SwiftUI
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+        
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         application.registerForRemoteNotifications()
         return true
     }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print("apns registered", deviceToken.base64EncodedString())
         self.sendDeviceTokenToServer(deviceToken: deviceToken)
     }
     
@@ -18,7 +18,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }
     
     private func sendDeviceTokenToServer(deviceToken: Data) {
-        // todo how to get our public key?
+        let publicKeyHex = CurrentUser.shared.publicKeyHex;
+        print("apns sending", publicKeyHex, deviceToken.base64EncodedString())
         // todo how to get our locale?
         // todo how to get our relays?
         // todo how to connect to a relay and send an event to it?
