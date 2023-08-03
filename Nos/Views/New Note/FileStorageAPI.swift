6923fee5d27c6df5593e527fe4a5b20ae5b7e908
boreq <boreq@0x46.net>
2023-08-03 19:40:24 +0200

    Error handling

--- Nos/Views/New Note/FileStorageAPI.swift
+++ Nos/Views/New Note/FileStorageAPI.swift
@@ -10,25 +10,65 @@ protocol FileStorageAPI {
 }
 
 class NostrBuildFileStorageAPI: FileStorageAPI {
-    var uploadURL = URL(string: "https://nostr.build/api/upload/ios.php")!
+    static let uploadURL = URL(string: "https://nostr.build/api/upload/ios.php")!
+    static let fileName = "file.jpg"
+    static let paramName = "fileToUpload"
     
     func upload(file: AttachedFile) async throws -> URL {
         let boundary = UUID().uuidString
-        let fileName = "file.jpg"
-        let paramName = "fileToUpload"
-        var request = URLRequest(url: uploadURL)
+        var request = URLRequest(url: Self.uploadURL)
         request.httpMethod = "POST"
         request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
         
+        var header = ""
+        header.append("\r\n--\(boundary)\r\n")
+        header.append("Content-Disposition: form-data; name=\"\(Self.paramName)\"; filename=\"\(Self.fileName)\"\r\n")
+        header.append("Content-Type: image/jpg\r\n\r\n")
+        
+        var trailer = ""
+        trailer.append("\r\n--\(boundary)--\r\n")
+
+        guard let headerData = header.data(using: .utf8), let trailerData = trailer.data(using: .utf8) else {
+            throw FileStorageAPIError.errorEncodingHeaderOrFooter
+        }
+        
+        guard let imageData = file.image.jpegData(compressionQuality: 85) else {
+            throw FileStorageAPIError.errorEncodingImage
+        }
+        
         var data = Data()
-        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-        data.append("Content-Disposition: form-data; name=\"\(paramName)\"; filename=\"\(fileName)\"\r\n".data(using: .utf8)!)
-        data.append("Content-Type: image/jpg\r\n\r\n".data(using: .utf8)!)
-        data.append(file.image.jpegData(compressionQuality: 85)!)
-        data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+        data.append(headerData)
+        data.append(imageData)
+        data.append(trailerData)
         
         let (responseData, _) = try await URLSession.shared.upload(for: request, from: data)
-        let url = try JSONSerialization.jsonObject(with: responseData, options: .allowFragments) as? String
-        return URL(string: url!)!
+        let urlString = try JSONSerialization.jsonObject(with: responseData, options: .allowFragments) as? String
+        guard let urlString else {
+            throw FileStorageAPIError.unexpectedOutputType
+        }
+        guard let url = URL(string: urlString) else {
+            throw FileStorageAPIError.couldNotParseURL(urlString)
+        }
+        return url
+    }
+}
+
+enum FileStorageAPIError: Error {
+    case unexpectedOutputType
+    case couldNotParseURL(String)
+    case errorEncodingHeaderOrFooter
+    case errorEncodingImage
+
+    var localizedDescription: String {
+        switch self {
+        case .unexpectedOutputType:
+            return "unexpected API output type"
+        case .couldNotParseURL(let urlString):
+            return "could not parse the string as url '\(urlString)'"
+        case .errorEncodingHeaderOrFooter:
+            return "error encoding multipart header or footer"
+        case .errorEncodingImage:
+            return "error encoding the image"
+        }
     }
 }
