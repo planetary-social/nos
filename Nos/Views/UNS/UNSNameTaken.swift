d898d6534337ea6825f7ae472c230d3bb64130ea
Matthew Lorentz <matt@lorentz.is>
2023-10-13 11:03:21 -0400

    Style the name taken screen

---
+++ Nos/Views/UNS/UNSNameTaken.swift
@@ -0,0 +1,18 @@
+//
+//  UNSNameTaken.swift
+//  Nos
+//
+//  Created by Matthew Lorentz on 10/13/23.
+//
+
+import SwiftUI
+
+struct UNSNameTaken: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UNSNameTaken()
+}
