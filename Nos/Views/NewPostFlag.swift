4300ddd130f52fa5a422c40f90988984ac8b9bfc
rabble <evan@protest.net>
2023-10-21 16:11:57 +1300

    adding new post flag experiment

---
+++ Nos/Views/NewPostFlag.swift
@@ -0,0 +1,42 @@
+//
+//  NewPostFlag.swift
+//  Nos
+//
+//  Created by Rabble on 10/20/23.
+//
+
+import SwiftUI
+
+struct NewPostFlagView: View {
+    var body: some View {
+        ZStack {
+            // Background gradient for RoundedRectangle
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .fill(LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color(red: 0.96, green: 0.25, blue: 0.46),
+                        Color(red: 0.94, green: 0.52, blue: 0.03)
+                    ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                ))
+                .frame(width:50, height: 20)
+            
+            // "Sign up" text
+            Text(Localized.new)
+                .lineLimit(1)
+                .font(.brand)
+                .foregroundColor(.primaryTxt)
+                .padding(2)
+        }
+    }
+}
+
+
+
+
+struct NewPostFlagView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPostFlagView()
+    }
+}
