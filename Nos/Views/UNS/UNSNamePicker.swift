85185fd1aa738be58e1503e199d78959406a8987
Matthew Lorentz <matt@lorentz.is>
2023-10-23 11:30:24 -0400

    TestFlight deployment

--- Nos/Views/UNS/UNSNamePicker.swift
+++ Nos/Views/UNS/UNSNamePicker.swift
@@ -114,6 +114,11 @@ struct UNSNamePicker: View {
                 .textInputAutocapitalization(.none)
                 .padding(15)
                 .offset(x: -6)
+                .onChange(of: desiredName) { 
+                    if desiredName.isEmpty == false {
+                        selectedName = nil
+                    }
+                }
                 
                 Spacer()
             }
