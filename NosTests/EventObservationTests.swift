04b5284b78e57e6644c296b8960c831bff236c30
Matthew Lorentz <matt@lorentz.is>
2024-01-22 11:13:56 -0500

    Disable EventObservationTests

--- NosTests/EventObservationTests.swift
+++ NosTests/EventObservationTests.swift
@@ -49,6 +49,7 @@ final class EventObservationTests: SQLiteStoreTestCase {
     /// This tests that the same event created in two separate contexts will update correctly in the view when both
     /// contexts are saved. This test exhibits bug https://github.com/planetary-social/nos/issues/697.  
     func testDuplicateEventMergingGivenParseContextSavesFirst() throws {
+        XCTExpectFailure("This test is failing intermittently, see #703", options: .nonStrict())
         // Arrange
         let viewContext = persistenceController.viewContext
         let parseContext = persistenceController.parseContext
