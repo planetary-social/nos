c118fdba626499a968f3a9eccfe142316c97c152
Matthew Lorentz <matt@lorentz.is>
2024-01-24 17:49:55 -0500

    Revert unecessary change in hydrateContactList

--- Nos/Models/CoreData/Event+CoreDataClass.swift
+++ Nos/Models/CoreData/Event+CoreDataClass.swift
@@ -669,12 +669,8 @@ public class Event: NosManagedObject {
         
         var newFollows = Set<Follow>()
         for jsonTag in jsonEvent.tags {
-            guard let followedKey = jsonTag[safe: 1],
-                followedKey.isValid else {
-                continue
-            }
-            
-            if let existingFollow = originalFollows[followedKey] {
+            if let followedKey = jsonTag[safe: 1], 
+                let existingFollow = originalFollows[followedKey] {
                 // We already have a Core Data Follow model for this user
                 newFollows.insert(existingFollow)
             } else {
