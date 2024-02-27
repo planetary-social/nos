5898ee46076a420425d582cbe1e00b1666ced1c3
Josh Brown <josh@nos.social>
2024-02-27 11:29:25 -0500

    remove unnecessary additional slash

--- Nos/Extensions/String+Markdown.swift
+++ Nos/Extensions/String+Markdown.swift
@@ -40,7 +40,7 @@ extension String {
         // The following pattern uses rules from the Domain Name System page on Wikipedia:
         // https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax,_internationalization
         // swiftlint:disable:next line_length
-        let regexPattern = "(\\s*)((https?://)?([a-zA-Z0-9][-a-zA-Z0-9]{0,62}\\.){1,127}[a-z]{2,63}\\b[-a-zA-Z0-9@:%_\\+.~#?&//=]*)"
+        let regexPattern = "(\\s*)((https?://)?([a-zA-Z0-9][-a-zA-Z0-9]{0,62}\\.){1,127}[a-z]{2,63}\\b[-a-zA-Z0-9@:%_\\+.~#?&/=]*)"
 
         do {
             let regex = try NSRegularExpression(pattern: regexPattern)
