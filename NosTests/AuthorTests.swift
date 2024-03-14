3a90519b0bbae2205b1bfc28ee49ab6945c8fbea
Martin Dutra <martindsq@hotmail.com>
2024-03-14 17:13:19 -0300

    Fix unit tests

--- NosTests/AuthorTests.swift
+++ NosTests/AuthorTests.swift
@@ -40,15 +40,18 @@ final class AuthorTests: CoreDataTestCase {
         XCTAssertEqual(fetchedAuthor?.followedKeys, [])
     }
 
-    func testMarkdownIdentifier() throws {
+    func testHumanFriendlyIdentifier() throws {
         let context = persistenceController.viewContext
         let key = RawNostrID.random
         let publicKey = try XCTUnwrap(PublicKey(hex: key))
         let truncatedKey = "\(publicKey.npub.prefix(10))..."
         let nip05 = "me@nip05.com"
         let author = try Author.findOrCreate(by: key, context: context)
-        XCTAssertEqual(author.markdownIdentifier, truncatedKey)
+        XCTAssertEqual(author.humanFriendlyIdentifier, truncatedKey)
         author.nip05 = nip05
-        XCTAssertEqual(author.markdownIdentifier, "[me@nip05.com](https://njump.me/\(publicKey.npub))")
+        XCTAssertEqual(author.humanFriendlyIdentifier, nip05)
+        let nip05WithUnderscore = "_@nip05.com"
+        author.nip05 = nip05WithUnderscore
+        XCTAssertEqual(author.humanFriendlyIdentifier, "nip05.com")
     }
 }
