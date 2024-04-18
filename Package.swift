a0df9db85432eb564d2ba4b828b76a9c12914e54
mplorentz <mplorentz@users.noreply.github.com>
2024-04-18 15:04:04 -0400

    Update Package.swift
    Co-authored-by: Josh Brown <josh@nos.social>

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swiftlint:disable:next file_header
 // swift-tools-version:5.2
 
-// this is here because it helps SourceKit-LSP parse your project i.e. when used with VSCode.
+// this file is here because it helps SourceKit-LSP parse your project i.e. when used with VSCode.
 // see https://medium.com/swlh/ios-development-on-vscode-27be37293fe1
 import PackageDescription
 let packageName = "Nos" 
