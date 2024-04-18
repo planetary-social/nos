55fc32182de83944546580d31d35a26090edae03
Matthew Lorentz <matt@lorentz.is>
2024-04-18 14:53:09 -0400

    Add comments to Package.swift to explain its use.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,10 @@
 // swiftlint:disable:next file_header
 // swift-tools-version:5.2
+
+// this is here because it helps SourceKit-LSP parse your project i.e. when used with VSCode.
+// see https://medium.com/swlh/ios-development-on-vscode-27be37293fe1
 import PackageDescription
-let packageName = "Nos" // <-- Change this to yours
+let packageName = "Nos" 
 let package = Package(
     name: "",
     // platforms: [.iOS("9.0")],
