248cbc4584db35c8f9b647d3673ef7f83c1b9949
Daniel Cadenas <dcadenas@gmail.com>
2024-04-25 13:22:44 -0300

    Refactor categories

--- Nos/Models/ReportCategory.swift
+++ Nos/Models/ReportCategory.swift
@@ -2,7 +2,6 @@ import Foundation
 
 /// A model for potential reasons why something might be reported.
 struct ReportCategory: Identifiable, Equatable {
-    
     /// A localized human readable description of the reason/category. Should be short enough to fit in an action menu.
     var displayName: String {
         String(localized: name)
@@ -27,9 +26,10 @@ struct ReportCategory: Identifiable, Equatable {
             for category in categories {
                 if category.code == code {
                     return category
-                } else if let subCategories = category.subCategories,
-                          let subCategory = searchForCategoryByCode?(code, subCategories) {
-                    return subCategory
+                } else if let subCategories = category.subCategories {
+                    if let subCategory = searchForCategoryByCode?(code, subCategories) {
+                        return subCategory
+                    }
                 }
             }
             
@@ -44,78 +44,211 @@ enum NIP56Code: String {
     case nudity, profanity, illegal, spam, impersonation, other
 }
 
-/// Vocabulary from [NIP-69](https://github.com/nostr-protocol/nips/pull/457).
-let coarseLanguage = ReportCategory(name: .moderation.coarseLanguage, code: "CL", nip56Code: .profanity)
-let likelyToCauseHarm = ReportCategory(name: .moderation.likelyToCauseHarm, code: "HC", nip56Code: .other)
-let intoleranceAndHate = ReportCategory(name: .moderation.intoleranceAndHate, code: "IH", nip56Code: .other)
-let illegal = ReportCategory(
-    name: .moderation.illegal,
-    code: "IL",
-    nip56Code: .illegal,
-    subCategories: [
-        ReportCategory(name: .moderation.copyrightViolation, code: "IL-cop", nip56Code: .illegal),
-        ReportCategory(name: .moderation.childSexualAbuse, code: "IL-csa", nip56Code: .illegal),
-        ReportCategory(name: .moderation.drugRelatedCrime, code: "IL-drg", nip56Code: .illegal),
-        ReportCategory(name: .moderation.fraudAndScams, code: "IL-frd", nip56Code: .illegal),
-        ReportCategory(name: .moderation.harassmentStalkingOrDoxxing, code: "IL-har", nip56Code: .illegal),
-        ReportCategory(name: .moderation.prostitution, code: "IL-swk", nip56Code: .illegal),
-        ReportCategory(name: .moderation.impersonation, code: "IL-idt", nip56Code: .illegal),
-        ReportCategory(name: .moderation.malware, code: "IL-mal", nip56Code: .illegal),
-    ]
-)
-let nudity = ReportCategory(
-    name: .moderation.nudityAndSex,
-    code: "NS",
-    nip56Code: .nudity,
-    subCategories: [
-        ReportCategory(name: .moderation.casualNudity, code: "NS-nud", nip56Code: .nudity),
-        ReportCategory(name: .moderation.erotica, code: "NS-ero", nip56Code: .nudity),
-        ReportCategory(name: .moderation.sex, code: "NS-sex", nip56Code: .nudity),
-    ]
-)
+enum ReportCategoryType {
+    static let coarseLanguage = ReportCategory(
+        name: .moderation.coarseLanguage,
+        code: "CL",
+        nip56Code: .profanity
+    )
+    
+    static let likelyToCauseHarm = ReportCategory(
+        name: .moderation.likelyToCauseHarm,
+        code: "HC",
+        nip56Code: .other
+    )
+    
+    static let intoleranceAndHate = ReportCategory(
+        name: .moderation.intoleranceAndHate,
+        code: "IH",
+        nip56Code: .other
+    )
+    
+    static let illegal = ReportCategory(
+        name: .moderation.illegal,
+        code: "IL",
+        nip56Code: .illegal,
+        subCategories: [
+            ReportSubCategoryType.copyrightViolation,
+            ReportSubCategoryType.childSexualAbuse,
+            ReportSubCategoryType.drugRelatedCrime,
+            ReportSubCategoryType.fraudAndScams,
+            ReportSubCategoryType.harassmentStalkingOrDoxxing,
+            ReportSubCategoryType.prostitution,
+            ReportSubCategoryType.impersonation,
+            ReportSubCategoryType.malware,
+        ]
+    )
+    static let nudity = ReportCategory(
+        name: .moderation.nudityAndSex,
+        code: "NS",
+        nip56Code: .nudity,
+        subCategories: [
+            ReportSubCategoryType.casualNudity,
+            ReportSubCategoryType.erotica,
+            ReportSubCategoryType.sex,
+        ]
+    )
+    
+    static let pornography = ReportCategory(
+        name: .moderation.pornography,
+        code: "PN",
+        nip56Code: .nudity,
+        subCategories: [
+            ReportSubCategoryType.heterosexualPorn,
+            ReportSubCategoryType.gayMalePorn,
+            ReportSubCategoryType.lesbianPorn,
+            ReportSubCategoryType.bisexualPorn,
+            ReportSubCategoryType.transsexualPorn,
+            ReportSubCategoryType.genderFluidNonBinaryPorn,
+        ]
+    )
+    static let spam = ReportCategory(name: .moderation.spam, code: "SP", nip56Code: .spam)
+    static let violence = ReportCategory(
+        name: .moderation.violence,
+        code: "VI",
+        nip56Code: .other,
+        subCategories: [
+            ReportSubCategoryType.violenceTowardsAHumanBeing,
+            ReportSubCategoryType.violenceTowardsASentientAnimal,
+        ]
+    )
+    static let other = ReportCategory(name: .moderation.other, code: "NA", nip56Code: .other)
+}
 
-let pornography = ReportCategory(
-    name: .moderation.pornography,
-    code: "PN",
-    nip56Code: .nudity,
-    subCategories: [
-        ReportCategory(name: .moderation.heterosexualPorn, code: "PN-het", nip56Code: .nudity),
-        ReportCategory(name: .moderation.gayMalePorn, code: "PN-gay", nip56Code: .nudity),
-        ReportCategory(name: .moderation.lesbianPorn, code: "PN-les", nip56Code: .nudity),
-        ReportCategory(name: .moderation.bisexualPorn, code: "PN-bis", nip56Code: .nudity),
-        ReportCategory(name: .moderation.transsexualPorn, code: "PN-trn", nip56Code: .nudity),
-        ReportCategory(name: .moderation.genderFluidNonBinaryPorn, code: "PN-fnb", nip56Code: .nudity),
-    ]
-)
-let spam = ReportCategory(name: .moderation.spam, code: "SP", nip56Code: .spam)
-let violence = ReportCategory(
-    name: .moderation.violence,
-    code: "VI",
-    nip56Code: .other,
-    subCategories: [
-        ReportCategory(name: .moderation.violenceTowardsAHumanBeing, code: "VI-hum", nip56Code: .other),
-        ReportCategory(name: .moderation.violenceTowardsASentientAnimal, code: "VI-ani", nip56Code: .other),
-    ]
-)
-let other = ReportCategory(name: .moderation.other, code: "NA", nip56Code: .other)
+enum ReportSubCategoryType {
+    static let copyrightViolation = ReportCategory(
+        name: .moderation.copyrightViolation,
+        code: "IL-cop",
+        nip56Code: .illegal
+    )
+    
+    static let childSexualAbuse = ReportCategory(
+        name: .moderation.childSexualAbuse,
+        code: "IL-csa",
+        nip56Code: .illegal
+    )
+    static let drugRelatedCrime = ReportCategory(
+        name: .moderation.drugRelatedCrime,
+        code: "IL-drg",
+        nip56Code: .illegal
+    )
+    
+    static let fraudAndScams = ReportCategory(
+        name: .moderation.fraudAndScams,
+        code: "IL-frd",
+        nip56Code: .illegal
+    )
+    
+    static let harassmentStalkingOrDoxxing = ReportCategory(
+        name: .moderation.harassmentStalkingOrDoxxing,
+        code: "IL-har",
+        nip56Code: .illegal
+    )
+    
+    static let prostitution = ReportCategory(
+        name: .moderation.prostitution,
+        code: "IL-swk",
+        nip56Code: .illegal
+    )
+    
+    static let impersonation = ReportCategory(
+        name: .moderation.impersonation,
+        code: "IL-idt",
+        nip56Code: .illegal
+    )
+    
+    static let malware = ReportCategory(
+        name: .moderation.malware,
+        code: "IL-mal",
+        nip56Code: .illegal
+    )
+    
+    static let casualNudity = ReportCategory(
+        name: .moderation.casualNudity,
+        code: "NS-nud",
+        nip56Code: .nudity
+    )
+    
+    static let erotica = ReportCategory(
+        name: .moderation.erotica,
+        code: "NS-ero",
+        nip56Code: .nudity
+    )
+    
+    // swiftlint:disable:next variable_name
+    static let sex = ReportCategory(
+        name: .moderation.sex,
+        code: "NS-sex",
+        nip56Code: .nudity
+    )
+    
+    static let heterosexualPorn = ReportCategory(
+        name: .moderation.heterosexualPorn,
+        code: "PN-het",
+        nip56Code: .nudity
+    )
+    
+    static let gayMalePorn = ReportCategory(
+        name: .moderation.gayMalePorn,
+        code: "PN-gay",
+        nip56Code: .nudity
+    )
+    
+    static let lesbianPorn = ReportCategory(
+        name: .moderation.lesbianPorn,
+        code: "PN-les",
+        nip56Code: .nudity
+    )
+    
+    static let bisexualPorn = ReportCategory(
+        name: .moderation.bisexualPorn,
+        code: "PN-bis",
+        nip56Code: .nudity
+    )
+    
+    static let transsexualPorn = ReportCategory(
+        name: .moderation.transsexualPorn,
+        code: "PN-trn",
+        nip56Code: .nudity
+    )
+    
+    static let genderFluidNonBinaryPorn = ReportCategory(
+        name: .moderation.genderFluidNonBinaryPorn,
+        code: "PN-fnb",
+        nip56Code: .nudity
+    )
+    
+    static let violenceTowardsAHumanBeing = ReportCategory(
+        name: .moderation.violenceTowardsAHumanBeing,
+        code: "VI-hum",
+        nip56Code: .other
+    )
+    
+    static let violenceTowardsASentientAnimal = ReportCategory(
+        name: .moderation.violenceTowardsASentientAnimal,
+        code: "VI-ani",
+        nip56Code: .other
+    )
+}
+
+/// Vocabulary from [NIP-69](https://github.com/nostr-protocol/nips/pull/457).
 
 let categories = [
-    coarseLanguage,
-    likelyToCauseHarm,
-    intoleranceAndHate,
-    illegal,
-    nudity,
-    pornography,
-    spam,
-    violence,
-    other
+    ReportCategoryType.coarseLanguage,
+    ReportCategoryType.likelyToCauseHarm,
+    ReportCategoryType.intoleranceAndHate,
+    ReportCategoryType.illegal,
+    ReportCategoryType.nudity,
+    ReportCategoryType.pornography,
+    ReportCategoryType.spam,
+    ReportCategoryType.violence,
+    ReportCategoryType.other,
 ]
 
 let selectableCategories = [
-    spam,
-    nudity,
-    coarseLanguage,
-    illegal,
-    other
+    ReportCategoryType.spam,
+    ReportCategoryType.nudity,
+    ReportCategoryType.coarseLanguage,
+    ReportCategoryType.illegal,
+    ReportCategoryType.other,
 ]
-
