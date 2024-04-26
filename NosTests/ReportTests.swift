f3921e55f3b79674c16f02bb4ea989676f44d725
Daniel Cadenas <dcadenas@gmail.com>
2024-04-26 17:24:46 -0300

    Fix text

--- NosTests/ReportTests.swift
+++ NosTests/ReportTests.swift
@@ -3,7 +3,7 @@ import XCTest
 final class ReportTests: XCTestCase {
 
     func testFindCategory() throws {
-        XCTAssertEqual(ReportCategory.findCategory(from: "PN"), categories[5])
-        XCTAssertEqual(ReportCategory.findCategory(from: "VI-hum"), categories[7].subCategories![0])
+        XCTAssertEqual(ReportCategory.findCategory(from: "PN"), allCategories[5])
+        XCTAssertEqual(ReportCategory.findCategory(from: "VI-hum"), allCategories[7].subCategories![0])
     }
 }
