e09afb57aa9c37367977fcdc61520e47ddb45094
mplorentz <mplorentz@users.noreply.github.com>
2024-06-18 10:04:10 -0400

    Disable Sentry to get project building in Xcode 16

--- Nos/Service/CrashReporting.swift
+++ Nos/Service/CrashReporting.swift
@@ -1,58 +1,57 @@
 import Foundation
-import Sentry
 
 /// An abstraction of an external crash reporting service, like Sentry.io
 class CrashReporting {
     
-    private let sentry: SentrySDK.Type
+//    private let sentry: SentrySDK.Type
     
     init(mock: Bool = false) {
-        sentry = SentrySDK.self
-        let dsn = Bundle.main.infoDictionary?["SENTRY_DSN"] as? String ?? ""
-
-        #if DEBUG
-        let debug = true
-        #else
-        let debug = false
-        #endif
-        
-        guard !mock && !debug && !dsn.isEmpty else {
-            return
-        }
-        
-        sentry.start { options in
-            options.dsn = dsn
-            #if STAGING
-            options.environment = "staging"
-            #elseif DEV
-            options.environment = "debug"
-            #endif
-            options.enableTracing = true
-            options.tracesSampleRate = 0.3 // tracing must be enabled for profiling
-            options.profilesSampleRate = 0.3 // see also `profilesSampler` if you need custom sampling logic
-            // Enable all experimental features
-            options.attachViewHierarchy = true
-            options.enablePreWarmedAppStartTracing = true
-            options.enableMetricKit = true
-            options.enableTimeToFullDisplayTracing = true
-            options.swiftAsyncStacktraces = true
-        }
+//        sentry = SentrySDK.self
+//        let dsn = Bundle.main.infoDictionary?["SENTRY_DSN"] as? String ?? ""
+//
+//        #if DEBUG
+//        let debug = true
+//        #else
+//        let debug = false
+//        #endif
+//        
+//        guard !mock && !debug && !dsn.isEmpty else {
+//            return
+//        }
+//        
+//        sentry.start { options in
+//            options.dsn = dsn
+//            #if STAGING
+//            options.environment = "staging"
+//            #elseif DEV
+//            options.environment = "debug"
+//            #endif
+//            options.enableTracing = true
+//            options.tracesSampleRate = 0.3 // tracing must be enabled for profiling
+//            options.profilesSampleRate = 0.3 // see also `profilesSampler` if you need custom sampling logic
+//            // Enable all experimental features
+//            options.attachViewHierarchy = true
+//            options.enablePreWarmedAppStartTracing = true
+//            options.enableMetricKit = true
+//            options.enableTimeToFullDisplayTracing = true
+//            options.swiftAsyncStacktraces = true
+//        }
     }
     
     func identify(with keyPair: KeyPair) {
-        let user = User(userId: keyPair.npub)
-        sentry.setUser(user)
+//        let user = User(userId: keyPair.npub)
+//        sentry.setUser(user)
     }
 
     func report(_ error: Error) {
-        sentry.capture(error: error)
+//        sentry.capture(error: error)
     }
     
     func report(_ errorMessage: String) {
-        sentry.capture(message: errorMessage)
+//        sentry.capture(message: errorMessage)
     }
     
     func logout() {
-        SentrySDK.setUser(nil)
+//        SentrySDK.setUser(nil)
     }
 }
