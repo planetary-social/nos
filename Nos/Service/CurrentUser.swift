99b15f32bba4e0d39c9057e03b2a380d3928ed56
Josh Brown <josh@nos.social>
2024-06-20 12:02:43 -0400

    fix swiftlint file length error

--- Nos/Service/CurrentUser.swift
+++ Nos/Service/CurrentUser.swift
@@ -3,23 +3,6 @@ import CoreData
 import Logger
 import Dependencies
 
-enum CurrentUserError: Error {
-    case authorNotFound
-    case encodingError
-    case errorWhilePublishingToRelays
-
-    var description: String? {
-        switch self {
-        case .authorNotFound:
-            return "Current user's author not found"
-        case .encodingError:
-            return "An encoding error happened while saving the user data"
-        case .errorWhilePublishingToRelays:
-            return "An encoding error happened while publishing to relays"
-        }
-    }
-}
-
 // swiftlint:disable type_body_length superfluous_disable_command
 @Observable class CurrentUser: NSObject, NSFetchedResultsControllerDelegate {
     
