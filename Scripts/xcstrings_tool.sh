f91ea239f056b46e208a1a6f47aaa1f497cedc1a
Josh Brown <josh@nos.social>
2024-06-25 13:09:12 -0400

    rename AssetsGenerated to Assets.generated

--- Scripts/xcstrings_tool.sh
+++ Scripts/xcstrings_tool.sh
@@ -1,7 +1,7 @@
-/Users/josh/.mint/bin/xcstrings-tool Nos/Assets/Localization/Localizable.xcstrings --output Nos/Assets/Generated/Localizable.generated.swift
+Scripts/xcstrings-tool/xcstrings-tool-0.5.1-macos/bin/xcstrings-tool Nos/Assets/Localization/Localizable.xcstrings --output Nos/Assets/Generated/Localizable.generated.swift
 
-/Users/josh/.mint/bin/xcstrings-tool ${PROJECT_DIR}/Nos/Assets/Localization/ImagePicker.xcstrings --output Nos/Assets/Generated/ImagePicker.generated.swift
+Scripts/xcstrings-tool/xcstrings-tool-0.5.1-macos/bin/xcstrings-tool Nos/Assets/Localization/ImagePicker.xcstrings --output Nos/Assets/Generated/ImagePicker.generated.swift
 
-/Users/josh/.mint/bin/xcstrings-tool ${PROJECT_DIR}/Nos/Assets/Localization/Moderation.xcstrings --output Nos/Assets/Generated/Moderation.generated.swift
+Scripts/xcstrings-tool/xcstrings-tool-0.5.1-macos/bin/xcstrings-tool Nos/Assets/Localization/Moderation.xcstrings --output Nos/Assets/Generated/Moderation.generated.swift
 
-/Users/josh/.mint/bin/xcstrings-tool ${PROJECT_DIR}/Nos/Assets/Localization/Reply.xcstrings --output Nos/Assets/Generated/Reply.generated.swift
+Scripts/xcstrings-tool/xcstrings-tool-0.5.1-macos/bin/xcstrings-tool Nos/Assets/Localization/Reply.xcstrings --output Nos/Assets/Generated/Reply.generated.swift
