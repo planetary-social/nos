eccdf904d6aaf5f53dc0abda707509e95eb2eab5
Josh Brown <josh@nos.social>
2024-07-05 17:32:36 -0400

    refactor code out of test and into new types

--- NosTests/Model/Bech32Tests.swift
+++ NosTests/Model/Bech32Tests.swift
@@ -40,39 +40,99 @@ final class Bech32Tests: CoreDataTestCase {
         print("decoded: \(decoded)")
         XCTAssertEqual(decoded.hrp, prefix)
 
-        let tlvString = try XCTUnwrap(TLV.decode(checksum: decoded.checksum))
-        print("tlvString: \(tlvString)")
-
-        let base8Data = try XCTUnwrap(try decoded.checksum.base8FromBase5())
-        let offset = 0
-        let type = base8Data[offset]
-        print("type: \(type)")
-        let length = base8Data[offset + 1]
-        let value = base8Data.subdata(in: offset + 2 ..< offset + 2 + Int(length))
-        print("value hex, which is the profile pubkey: \(value.hexString)")
-
-        let pubkey = value // or tlvString
-        XCTAssertEqual(pubkey.hexString, "3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d")
-        XCTAssertEqual(pubkey.hexString, tlvString)
-
-        let nextOffset = offset + 2 + Int(length)
-        let nextType = base8Data[nextOffset]
-        print("nextType: \(nextType)")
-        let nextLength = base8Data[nextOffset + 1]
-        let nextValue = base8Data.subdata(in: nextOffset + 2 ..< nextOffset + 2 + Int(nextLength))
-        print("nextValue hex: \(nextValue.hexString)")
-
-        let firstRelay = String(data: nextValue, encoding: .ascii)
-        XCTAssertEqual(firstRelay, "wss://r.x.com")
-
-        let thirdOffset = nextOffset + 2 + Int(nextLength)
-        let thirdType = base8Data[thirdOffset]
-        print("thirdType: \(thirdType)")
-        let thirdLength = base8Data[thirdOffset + 1]
-        let thirdValue = base8Data.subdata(in: thirdOffset + 2 ..< thirdOffset + 2 + Int(thirdLength))
-        print("thirdValue hex: \(thirdValue.hexString)")
-
-        let secondRelay = String(data: thirdValue, encoding: .ascii)
-        XCTAssertEqual(secondRelay, "wss://djbas.sadkb.com")
+        let entity = try Bech32Entity.decode(bech32String: nprofile)
+        switch entity {
+        case .nprofile(let publicKey, let relays):
+            XCTAssertEqual(publicKey, "3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d")
+            XCTAssertEqual(relays.count, 2)
+            let firstRelay = try XCTUnwrap(relays.first)
+            XCTAssertEqual(firstRelay, "wss://r.x.com")
+            let secondRelay = try XCTUnwrap(relays.last)
+            XCTAssertEqual(secondRelay, "wss://djbas.sadkb.com")
+        default:
+            XCTFail("Expected to get a nprofile")
+        }
     }
+}
+
+enum Bech32EntityError: Error {
+    case unknownFormat
+    case unknownPrefix
+}
+
+enum Bech32Entity {
+    case nprofile(publicKey: String, relays: [String])
+    case nevent // TODO: add associated values
+
+    static func decode(bech32String: String) throws -> Bech32Entity {
+        let (humanReadablePart, data) = try Bech32.decode(bech32String)
+        switch humanReadablePart {
+        case "nprofile":
+            let profile = try decodeNprofile(data: data)
+            return .nprofile(publicKey: profile.publicKey, relays: profile.relays)
+        default:
+            throw Bech32EntityError.unknownPrefix
+        }
+    }
+
+    private static func decodeNprofile(data: Data) throws -> (publicKey: String, relays: [String]) {
+        let tlvEntities = TLVEntity.decodeEntities(data: data)
+
+        guard let publicKey = (tlvEntities.first { $0.type == .special }.map { $0.value }) else {
+            throw Bech32EntityError.unknownFormat
+        }
+        let relays = tlvEntities.filter { $0.type == .relay }.map { $0.value }
+        
+        return (publicKey, relays)
+    }
+}
+
+struct TLVEntity {
+    let type: TLVType
+    let length: Int
+    let value: String
+
+    static func decodeEntities(data: Data) -> [TLVEntity] {
+        guard let converted = try? data.base8FromBase5() else {
+            return []
+        }
+
+        var result: [TLVEntity] = []
+
+        var offset = 0
+        while offset + 1 < converted.count {
+            let rawType = converted[offset]
+            let type = TLVType(rawValue: rawType)
+            let length = Int(converted[offset + 1])
+            let value = converted.subdata(in: offset + 2 ..< offset + 2 + length)
+
+            switch type {
+            case .special:
+                let valueString = SHA256Key.decode(base8: value)
+                let entity = TLVEntity(type: .special, length: length, value: valueString)
+                result.append(entity)
+            case .relay:
+                let valueString = String(data: value, encoding: .ascii)
+                let entity = TLVEntity(type: .relay, length: length, value: valueString ?? "") // TODO: handle error
+                result.append(entity)
+            case .author:
+                break // TODO: support author
+            case .kind:
+                break // TODO: support kind
+            case nil:
+                break // TODO: hmmm...we have an issue reading the type for this entity; this is bad
+            }
+
+            offset += length + 2
+        }
+
+        return result
+    }
+}
+
+enum TLVType: UInt8 {
+    case special = 0
+    case relay = 1
+    case author = 2
+    case kind = 3
 }
