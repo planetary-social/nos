93c14ac452969ea91bca600f4eb98164d5f6fddf
Josh Brown <josh@nos.social>
2024-08-15 17:07:18 -0400

    simpler version using scroll view

--- Nos/Views/Components/Images/ImageViewer.swift
+++ Nos/Views/Components/Images/ImageViewer.swift
@@ -8,101 +8,40 @@ struct ImageViewer: View {
 
     @Environment(\.dismiss) var dismiss
 
-    @State private var scale: CGFloat = 1.0
-    @State private var lastScale: CGFloat = 1.0
+    @State private var zoomScale: CGFloat = 1.0
+    @State private var previousZoomScale: CGFloat = 1
     @State private var offset: CGSize = .zero
     @State private var lastOffset: CGSize = .zero
     @State private var imageSize: CGSize = .zero
     @State private var anchor: UnitPoint = .center
 
-    private let maxScale: CGFloat = 10.0
-    private let minScale: CGFloat = 1.0
+    private let maxZoomScale: CGFloat = 10.0
+    private let minZoomScale: CGFloat = 1.0
 
     var body: some View {
         ZStack {
             Color.imageBackground
 
-            GeometryReader { geometry in
-                WebImage(url: url)
-                    .onSuccess { image, _, _ in
-                        imageSize = image.size
-                    }
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .scaleEffect(scale, anchor: anchor)
-                    .offset(x: offset.width, y: offset.height)
-                    .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                withAnimation {
-                                    anchor = .center
-                                    offset = CGSize(
-                                        width: lastOffset.width + value.translation.width,
-                                        height: lastOffset.height + value.translation.height
-                                    )
-                                }
-                            }
-                            .onEnded { value in
-                                withAnimation {
-                                    let predictedEndOffset = CGSize(
-                                        width: lastOffset.width + value.predictedEndTranslation.width,
-                                        height: lastOffset.height + value.predictedEndTranslation.height
-                                    )
-                                    offset = predictedEndOffset
-
-                                    keepImageOnScreen(geometry: geometry)
-
-                                    lastOffset = offset
-                                }
-                            }
-                            .simultaneously(
-                                with: MagnifyGesture()
-                                    .onChanged { value in
-                                        withAnimation {
-                                            scale = lastScale * value.magnification
-                                            anchor = value.startAnchor
-                                        }
-                                    }
-                                    .onEnded { value in
-                                        withAnimation {
-                                            let newScale = lastScale * value.magnification
-                                            if newScale > maxScale {
-                                                scale = maxScale
-                                            } else if newScale < minScale {
-                                                scale = minScale
-                                            }
-
-                                            lastScale = scale
-                                            keepImageOnScreen(geometry: geometry)
-                                            lastOffset = offset
-                                        }
-                                    }
-                            )
-                            .simultaneously(
-                                with: SpatialTapGesture(count: 2)
-                                    .onEnded { value in
-                                        withAnimation {
-                                            let newAnchor = UnitPoint(
-                                                x: value.location.x / geometry.size.width,
-                                                y: value.location.y / geometry.size.height
-                                            )
-
-                                            if scale == minScale {
-                                                scale = 4.0
-                                                anchor = newAnchor
-                                            } else {
-                                                scale = minScale
-                                                anchor = .center
-                                                offset = .zero
-                                                lastOffset = .zero
-                                            }
-                                            lastScale = scale
-                                        }
-                                    }
-                            )
-                    )
-                    .frame(width: geometry.size.width, height: geometry.size.height)
-                    .clipped()
+            GeometryReader { proxy in
+                ScrollView(
+                    [.vertical, .horizontal],
+                    showsIndicators: false
+                ) {
+                    WebImage(url: url)
+                        .onSuccess { image, _, _ in
+                            imageSize = image.size
+                        }
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .gesture(
+                            zoomGesture
+                                .simultaneously(with: doubleTapGesture)
+                        )
+                        .scaleEffect(CGSize(width: 1.0, height: 1.0), anchor: anchor)
+                        .frame(width: proxy.size.width * max(minZoomScale, zoomScale))
+                        .frame(maxHeight: .infinity)
+                }
+                .defaultScrollAnchor(anchor)
             }
 
             ZStack(alignment: .topLeading) {
@@ -125,56 +64,54 @@ struct ImageViewer: View {
         }
     }
 
-    func keepImageOnScreen(geometry: GeometryProxy) {
-        let imageRatio = imageSize.width / imageSize.height
-        let geometryRatio = geometry.size.width / geometry.size.height
-        if imageRatio > geometryRatio { // the image width is constrained by the screen width
-            let scaledImageWidth = geometry.size.width * scale
-            let horizontalPanningRange = scaledImageWidth - geometry.size.width
-            let maxWidthOffset = horizontalPanningRange / 2
-            let minWidthOffset = -maxWidthOffset
-
-            if offset.width < minWidthOffset {
-                offset.width = minWidthOffset
-            } else if offset.width > maxWidthOffset {
-                offset.width = maxWidthOffset
-            }
-
-            let scaledImageHeight =
-                (geometry.size.width / imageSize.width) * imageSize.height * scale
-            let verticalPanningRange = max(scaledImageHeight - geometry.size.height, 0)
-            let maxHeightOffset = verticalPanningRange / 2
-            let minHeightOffset = -maxHeightOffset
-
-            if offset.height < minHeightOffset {
-                offset.height = minHeightOffset
-            } else if offset.height > maxHeightOffset {
-                offset.height = maxHeightOffset
-            }
-        } else { // the image height is constrained by the screen height
-            let scaledImageHeight = geometry.size.height * scale
-            let verticalPanningRange = scaledImageHeight - geometry.size.height
-            let maxHeightOffset = verticalPanningRange / 2
-            let minHeightOffset = -maxHeightOffset
-
-            if offset.height < minHeightOffset {
-                offset.height = minHeightOffset
-            } else if offset.height > maxHeightOffset {
-                offset.height = maxHeightOffset
-            }
-
-            let scaledImageWidth =
-                (geometry.size.height / imageSize.height) * imageSize.width * scale
-            let horizontalPanningRange = max(scaledImageWidth - geometry.size.width, 0)
-            let maxWidthOffset = horizontalPanningRange / 2
-            let minWidthOffset = -maxWidthOffset
-
-            if offset.width < minWidthOffset {
-                offset.width = minWidthOffset
-            } else if offset.width > maxWidthOffset {
-                offset.width = maxWidthOffset
-            }
+    func resetImageState() {
+        withAnimation(.interactiveSpring()) {
+            zoomScale = 1
+            anchor = .center
         }
+    }
+
+    func onImageDoubleTapped(value: TapGesture.Value) {
+        if zoomScale == 1 {
+            withAnimation(.spring()) {
+                zoomScale = 4
+            }
+        } else {
+            resetImageState()
+        }
+    }
+
+    func onZoomGestureStarted(value: MagnifyGesture.Value) {
+        withAnimation(.easeIn(duration: 0.1)) {
+            let delta = value.magnification / previousZoomScale
+            previousZoomScale = value.magnification
+            let zoomDelta = zoomScale * delta
+            var minMaxScale = max(minZoomScale, zoomDelta)
+            minMaxScale = min(maxZoomScale, minMaxScale)
+            zoomScale = minMaxScale
+            anchor = value.startAnchor
+        }
+    }
+
+    func onZoomGestureEnded(value: MagnifyGesture.Value) {
+        previousZoomScale = 1
+        anchor = value.startAnchor
+        if zoomScale <= 1 {
+            resetImageState()
+        } else if zoomScale > 5 {
+            zoomScale = 5
+        }
+    }
+
+    var zoomGesture: some Gesture {
+        MagnifyGesture()
+            .onChanged(onZoomGestureStarted)
+            .onEnded(onZoomGestureEnded)
+    }
+
+    var doubleTapGesture: some Gesture {
+        TapGesture(count: 2)
+            .onEnded(onImageDoubleTapped)
     }
 }
 
