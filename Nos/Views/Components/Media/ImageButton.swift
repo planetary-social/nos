9fc420c9b4117ea522bf6730b808336979787e0c
Josh Brown <josh@nos.social>
2024-08-22 14:40:33 -0400

    a few options for aspect ratios, but one good one

--- Nos/Views/Components/Media/ImageButton.swift
+++ Nos/Views/Components/Media/ImageButton.swift
@@ -9,7 +9,15 @@ struct ImageButton: View {
     /// Whether the image viewer is presented or not.
     @State private var isViewerPresented = false
 
+    @State private var image: PlatformImage?
+
     var body: some View {
+//        workingWebImage
+//        rectangularImage
+        frameImage
+    }
+
+    var frameImage: some View {
         Button {
             isViewerPresented = true
         } label: {
@@ -17,11 +25,85 @@ struct ImageButton: View {
                 .resizable()
                 .indicator(.activity)
                 .aspectRatio(contentMode: .fill)
-                .clipped()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    minHeight: 0,
+                    maxHeight: .infinity
+                )
+                .aspectRatio(4 / 3, contentMode: .fit)
+                .clipShape(.rect)
         }
         .sheet(isPresented: $isViewerPresented) {
             ImageViewer(url: url)
+        }
+    }
+
+    var rectangularImage: some View {
+        Color.clear
+            .aspectRatio(1, contentMode: .fit)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay {
+                WebImage(url: url)
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: .fill)
+                    .onTapGesture {
+                        isViewerPresented = true
+                    }
+            }
+        .clipShape(Rectangle())
+    }
+
+    var workingWebImage: some View {
+        GeometryReader { proxy in
+            WebImage(url: url)
+                .onSuccess { image, _, _ in
+                    self.image = image
+                }
+                .resizable()
+                .indicator(.activity)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: proxy.size.width, height: proxy.size.width * 3 / 4)
+                .clipped()
+                .sheet(isPresented: $isViewerPresented) {
+                    ImageViewer(url: url)
+                }
+        }
+    }
+
+    @ViewBuilder var workingImage: some View {
+        if let image {
+            GeometryReader { proxy in
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: proxy.size.width, height: proxy.size.width * 3 / 4)
+                    .clipped()
+            }
+        } else {
+            EmptyView()
+        }
+    }
+
+    @ViewBuilder var triedABunchOfThingsImage: some View {
+        if let image {
+            GeometryReader { proxy in
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                //                .aspectRatio(contentMode: .fit)
+                //                .scaledToFit()
+                //                    .aspectRatio(CGSize(width: 4, height: 3), contentMode: .fill)
+                    .frame(width: proxy.size.width, height: proxy.size.width * 3 / 4)
+                    .clipped()
+                //                .clipShape(
+                //                    Rectangle()
+                //                        .aspectRatio(CGSize(width: 4, height: 3), contentMode: .fill)
+                //                )
+            }
+        } else {
+            EmptyView()
         }
     }
 }
