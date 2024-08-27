1c4d4a2b003e8127bd929817e199a35a71b65230
Bryan Montz <bryanmontz@me.com>
2024-08-27 08:26:06 -0500

    remove unnecessary test

--- NosTests/Extensions/AttributedString+QuotationsTests.swift
+++ NosTests/Extensions/AttributedString+QuotationsTests.swift
@@ -22,13 +22,4 @@ final class AttributedString_QuotationsTests: XCTestCase {
         let wrapped = content.wrappingWithQuotationMarks(locale: Locale(identifier: localeIdentifier))
         return String(wrapped.characters)
     }
-    
-    func testExistingAttributesMaintainedForInsertedCharacters() {
-        let expectedAttributes = AttributeContainer([.foregroundColor: UIColor.blue])
-        let content = AttributedString("test", attributes: expectedAttributes)
-        
-        let wrapped = content.wrappingWithQuotationMarks(locale: Locale(identifier: "en"))
-        XCTAssertEqual(wrapped.runs.first?.attributes, expectedAttributes)
-        XCTAssertEqual(wrapped.runs.last?.attributes, expectedAttributes)
-    }
 }
