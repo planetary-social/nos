f9dbd53d2d11b342993613a3a666789591f03ef8
Itunu Raimi <chavitunu@gmail.com>
2024-09-04 16:12:34 +0100

    change name to display name in ProfileEditView

--- Nos/Views/ProfileEdit/ProfileEditView.swift
+++ Nos/Views/ProfileEdit/ProfileEditView.swift
@@ -61,7 +61,7 @@ struct ProfileEditView: View {
             }
             
             NosFormSection(label: .localizable.basicInfo) {
-                NosTextField(label: .localizable.name, text: $nameText)
+                NosTextField(label: .localizable.displayName, text: $nameText)
                 FormSeparator()
                 if author.hasNosNIP05 {
                     NosNIP05Field(
