3cd2570a23bfc87e5384d3265fd89a260b5180b2
Itunu Raimi <chavitunu@gmail.com>
2024-09-10 17:48:37 +0100

    make toggke string optional

--- Nos/Views/Components/NosToggle.swift
+++ Nos/Views/Components/NosToggle.swift
@@ -1,13 +1,16 @@
 import SwiftUI
-
+/// A toggle with the tint color set to green.
 struct NosToggle: View {
     @Binding var isOn: Bool
-    var labelText: LocalizedStringResource
+    /// Optional, in case we need to use a toggle without a string
+    var labelText: LocalizedStringResource?
 
     var body: some View {
         Toggle(isOn: $isOn) {
-            Text(labelText)
-                .foregroundColor(.primaryTxt)
+            if let labelText = labelText {
+                Text(labelText)
+                    .foregroundColor(.primaryTxt)
+            }
         }
         .tint(.green)
     }
