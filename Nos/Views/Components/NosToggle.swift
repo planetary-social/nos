7187a7bd85e37d8297769b9b2f569ab884938ffa
Itunu Raimi <chavitunu@gmail.com>
2024-09-10 20:05:08 +0100

    update comment

--- Nos/Views/Components/NosToggle.swift
+++ Nos/Views/Components/NosToggle.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 /// A toggle with the tint color set to green.
+/// - Note: Fixes [#1251](https://github.com/planetary-social/nos/issues/1251)
 struct NosToggle: View {
     @Binding var isOn: Bool
     /// A string that shows up beside the toggle. Optional.
