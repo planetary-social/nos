4e0a0bfbc1a2d1e5fd3e068c13ec3c94a4fa10fc
Itunu Raimi <chavitunu@gmail.com>
2024-09-11 19:06:54 +0100

    refactor featureflags file

--- Nos/Service/FeatureFlags.swift
+++ Nos/Service/FeatureFlags.swift
@@ -1,17 +1,25 @@
 import Foundation
 import Dependencies
 
-/// The set of feature flags used by the app.
-protocol FeatureFlags {
+/// Enum to represent each feature flag. Add new feature flags here easily.
+enum FeatureFlag: String {
     /// Whether the new media display should be enabled or not.
     /// - Note: See [#1177](https://github.com/planetary-social/nos/issues/1177) for details on the new media display.
-    var newMediaDisplayEnabled: Bool { get }
+    case newMediaDisplay
+    /// Whether the new moderation flow should be enabled or not.
+    /// - Note: See [#1489](https://github.com/planetary-social/nos/issues/1489) for details on the new moderation flow.
+    case newModerationFlow
+}
+
+/// The set of feature flags used by the app.
+protocol FeatureFlags {
+    /// Retrieves the value of the specified feature flag.
+    func isEnabled(_ feature: FeatureFlag) -> Bool
 
     // MARK: - Additional requirements for debug mode
-
     #if DEBUG || STAGING
-    /// Sets the value of `newMediaDisplayEnabled`.
-    func setNewMediaDisplayEnabled(_ enabled: Bool)
+    /// Sets the value of the specified feature flag.
+    func setEnabled(_ feature: FeatureFlag, enabled: Bool)
     #endif
 }
 
@@ -22,13 +30,19 @@ class DefaultFeatureFlags: FeatureFlags, DependencyKey {
 
     private init() {}
 
-    private(set) var newMediaDisplayEnabled = false
-}
+    /// A dictionary to store the values of feature flags.
+    private var featureFlags: [FeatureFlag: Bool] = [
+        .newMediaDisplay: false,
+        .newModerationFlow: false
+    ]
 
-#if DEBUG || STAGING
-extension DefaultFeatureFlags {
-    func setNewMediaDisplayEnabled(_ enabled: Bool) {
-        newMediaDisplayEnabled = enabled
+    func isEnabled(_ feature: FeatureFlag) -> Bool {
+        featureFlags[feature] ?? false
     }
+
+    #if DEBUG || STAGING
+    func setEnabled(_ feature: FeatureFlag, enabled: Bool) {
+        featureFlags[feature] = enabled
+    }
+    #endif
 }
-#endif
