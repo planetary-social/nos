f7eb2440562a1c3db17032cfb6daa085892fdc68
Itunu Raimi <chavitunu@gmail.com>
2024-09-11 19:07:12 +0100

    refactor mock feature flags

--- NosTests/Service/MockFeatureFlags.swift
+++ NosTests/Service/MockFeatureFlags.swift
@@ -1,8 +1,16 @@
 /// A set of feature flag values used for testing that can be customized.
 class MockFeatureFlags: FeatureFlags {
-    var newMediaDisplayEnabled = false
+    /// A dictionary to store the mock values for feature flags.
+    private var featureFlags: [FeatureFlag: Bool] = [
+        .newMediaDisplay: false,
+        .newModerationFlow: false
+    ]
 
-    func setNewMediaDisplayEnabled(_ enabled: Bool) {
-        newMediaDisplayEnabled = enabled
+    func isEnabled(_ feature: FeatureFlag) -> Bool {
+        featureFlags[feature] ?? false
+    }
+    
+    func setEnabled(_ feature: FeatureFlag, enabled: Bool) {
+        featureFlags[feature] = enabled
     }
 }
