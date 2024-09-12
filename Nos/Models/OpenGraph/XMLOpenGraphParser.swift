c2d4f2f29f7a467c386814a094114e136dbeb915
Josh Brown <josh@nos.social>
2024-09-12 16:33:30 -0400

    parse video height, too!

--- Nos/Models/OpenGraph/XMLOpenGraphParser.swift
+++ Nos/Models/OpenGraph/XMLOpenGraphParser.swift
@@ -1,26 +1,36 @@
 import Foundation
+import Logger
 
 class XMLOpenGraphParser: NSObject, OpenGraphParser, XMLParserDelegate {
-    private var videoWidth: String?
+    private var videoWidth: Double?
+    private var videoHeight: Double?
 
     func videoMetadata(html: Data) -> OpenGraphMedia? {
-        let width = videoWidth(data: html)
-        return OpenGraphMedia(url: nil, type: .video, width: width, height: nil)
+        parse(data: html)
+        return OpenGraphMedia(url: nil, type: .video, width: videoWidth, height: videoHeight)
     }
 
-    func videoWidth(data: Data) -> Double? {
+    private func parse(data: Data) {
         let parser = XMLParser(data: data)
         parser.delegate = self
         parser.parse()
-
-        guard let videoWidth else { return nil }
-        return Double(videoWidth)
     }
 
-    // XMLParserDelegate methods
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        if elementName == "meta", let property = attributeDict["property"], property == "og:video:width" {
-            videoWidth = attributeDict["content"]
+    // MARK: - XMLParserDelegate
+
+    func parser(
+        _ parser: XMLParser,
+        didStartElement elementName: String,
+        namespaceURI: String?,
+        qualifiedName qName: String?,
+        attributes attributeDict: [String: String] = [:]
+    ) {
+        if elementName == "meta", let property = attributeDict["property"], let content = attributeDict["content"] {
+            if property == "og:video:width" {
+                videoWidth = Double(content)
+            } else if property == "og:video:height" {
+                videoHeight = Double(content)
+            }
         }
     }
 
@@ -28,10 +38,7 @@ class XMLOpenGraphParser: NSObject, OpenGraphParser, XMLParserDelegate {
         // No need to handle characters for meta tags
     }
 
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-    }
-
     func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
-        print("Parse error: \(parseError.localizedDescription)")
+        Log.error("Parse error: \(parseError.localizedDescription)")
     }
 }
