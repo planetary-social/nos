40912c511225ebb7ef7309384146d956a01207c3
Bryan Montz <bryanmontz@me.com>
2024-09-15 08:31:46 -0500

    added support for NIP-62 Request to Vanish events #80

--- NosTests/Models/JSONEventTests.swift
+++ NosTests/Models/JSONEventTests.swift
@@ -14,4 +14,41 @@ class JSONEventTests: XCTestCase {
         // Act & Assert
         XCTAssertEqual(subject.replaceableID, replaceableID)
     }
+    
+    func test_requestToVanish_fromSpecificRelays() {
+        let event = JSONEvent.requestToVanish(
+            pubKey: "",
+            relays: [
+                URL(string: "wss://relay1.lol")!,
+                URL(string: "wss://relay2.lol")!,
+                URL(string: "wss://relay3.lol")!
+            ],
+            reason: "I'm done with this."
+        )
+        
+        let expectedTags = [
+            ["relay", "wss://relay1.lol"],
+            ["relay", "wss://relay2.lol"],
+            ["relay", "wss://relay3.lol"]
+        ]
+        
+        XCTAssertEqual(event.kind, 62)
+        XCTAssertEqual(event.tags, expectedTags)
+        XCTAssertEqual(event.content, "I'm done with this.")
+    }
+    
+    func test_requestToVanish_fromAllRelays() {
+        let event = JSONEvent.requestToVanish(
+            pubKey: "",
+            reason: "I'm done with this."
+        )
+        
+        let expectedTags = [
+            ["relay", "ALL_RELAYS"]
+        ]
+        
+        XCTAssertEqual(event.kind, 62)
+        XCTAssertEqual(event.tags, expectedTags)
+        XCTAssertEqual(event.content, "I'm done with this.")
+    }
 }
