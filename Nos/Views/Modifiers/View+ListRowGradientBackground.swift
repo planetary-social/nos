cbdbf71a2ba8e2f011db54a462e135fc4e5ef047
Bryan Montz <bryanmontz@me.com>
2024-09-17 06:14:04 -0500

    added doc comments

--- Nos/Views/Modifiers/View+ListRowGradientBackground.swift
+++ Nos/Views/Modifiers/View+ListRowGradientBackground.swift
@@ -1,11 +1,13 @@
 import SwiftUI
 
 extension View {
+    /// Applies a gradient background to list rows.
     func listRowGradientBackground() -> some View {
         modifier(ListRowGradientBackground())
     }
 }
 
+/// A gradient background to apply to list rows.
 struct ListRowGradientBackground: ViewModifier {
     func body(content: Content) -> some View {
         content
