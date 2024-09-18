51bfb0bebdbfc891768a81c514d4510125ef4b09
Josh Brown <josh@nos.social>
2024-09-18 16:22:35 -0400

    #1165: Parse more Open Graph metadata

--- NosTests/Models/OpenGraph/SoupOpenGraphParserTests.swift
+++ NosTests/Models/OpenGraph/SoupOpenGraphParserTests.swift
@@ -2,19 +2,6 @@ import Foundation
 import XCTest
 
 class SoupOpenGraphParserTests: XCTestCase {
-    let sampleHTML = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta property="og:video:width" content="2560">
-        <meta property="og:video:height" content="1440">
-    </head>
-    <body>
-        <h1>Sample HTML</h1>
-    </body>
-    </html>
-    """
-
     // swiftlint:disable:next implicitly_unwrapped_optional
     var youTubeHTML: Data!
 
@@ -22,17 +9,71 @@ class SoupOpenGraphParserTests: XCTestCase {
         youTubeHTML = try htmlData(filename: "youtube_video_toxic")
     }
 
-    func test_parse() throws {
+    func test_parse_sample_video() throws {
         // Arrange
         let parser = SoupOpenGraphParser()
-        let data = try XCTUnwrap(sampleHTML.data(using: .utf8))
+        let data = try XCTUnwrap(sampleVideoHTML.data(using: .utf8))
 
         // Act
-        let videoMetadata = try XCTUnwrap(parser.videoMetadata(html: data))
+        let metadata = try XCTUnwrap(parser.metadata(html: data))
 
         // Assert
-        XCTAssertEqual(videoMetadata.width, 2560)
-        XCTAssertEqual(videoMetadata.height, 1440)
+        XCTAssertEqual(metadata.type, .video)
+        
+        XCTAssertEqual(metadata.video?.url, "https://example.com/movie.swf")
+        XCTAssertEqual(metadata.video?.width, 2560)
+        XCTAssertEqual(metadata.video?.height, 1440)
+
+        XCTAssertEqual(metadata.image?.url, "https://example.com/rock.jpg")
+        XCTAssertEqual(metadata.image?.width, 1280)
+        XCTAssertEqual(metadata.image?.height, 720)
+    }
+
+    func test_parse_sample_video_missing_metadata() throws {
+        // Arrange
+        let parser = SoupOpenGraphParser()
+        let data = try XCTUnwrap(sampleVideoMissingMetadataHTML.data(using: .utf8))
+
+        // Act
+        let metadata = try XCTUnwrap(parser.metadata(html: data))
+
+        // Assert
+        XCTAssertEqual(metadata.type, .video)
+
+        XCTAssertNil(metadata.video?.url)
+        XCTAssertEqual(metadata.video?.width, 1280)
+        XCTAssertNil(metadata.video?.height)
+
+        XCTAssertNil(metadata.image)
+    }
+
+    func test_parse_sample_website() throws {
+        // Arrange
+        let parser = SoupOpenGraphParser()
+        let data = try XCTUnwrap(sampleWebsiteHTML.data(using: .utf8))
+
+        // Act
+        let metadata = try XCTUnwrap(parser.metadata(html: data))
+
+        // Assert
+        XCTAssertEqual(metadata.type, .website)
+
+        XCTAssertEqual(metadata.image?.url, "https://example.com/rock.jpg")
+        XCTAssertEqual(metadata.image?.width, 640)
+        XCTAssertEqual(metadata.image?.height, 480)
+    }
+
+    func test_parse_sample_website_with_missing_metadata() throws {
+        // Arrange
+        let parser = SoupOpenGraphParser()
+        let data = try XCTUnwrap(sampleWebsiteMissingMetadataHTML.data(using: .utf8))
+
+        // Act
+        let metadata = try XCTUnwrap(parser.metadata(html: data))
+
+        // Assert
+        XCTAssertEqual(metadata.type, .unknown)
+        XCTAssertEqual(metadata.image?.width, 640)
     }
 
     func test_parse_youTube() throws {
@@ -41,10 +82,86 @@ class SoupOpenGraphParserTests: XCTestCase {
         let data = try XCTUnwrap(youTubeHTML)
 
         // Act
-        let videoMetadata = try XCTUnwrap(parser.videoMetadata(html: data))
+        let metadata = try XCTUnwrap(parser.metadata(html: data))
 
         // Assert
-        XCTAssertEqual(videoMetadata.width, 1280)
-        XCTAssertEqual(videoMetadata.height, 720)
+        XCTAssertEqual(metadata.title, "Toxic - Vintage 1930s Torch Song Britney Spears Cover ft. Melinda Doolittle")
+
+        XCTAssertEqual(metadata.type, .video)
+
+        XCTAssertEqual(metadata.video?.url, "https://www.youtube.com/embed/ZILsHowUjpQ")
+        XCTAssertEqual(metadata.video?.width, 1280)
+        XCTAssertEqual(metadata.video?.height, 720)
+        
+        XCTAssertEqual(metadata.image?.width, 1280)
+        XCTAssertEqual(metadata.image?.height, 720)
+    }
+}
+
+extension SoupOpenGraphParserTests {
+    var sampleVideoHTML: String {
+        """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta property="og:type" content="video.movie">
+            <meta property="og:video" content="https://example.com/movie.swf">
+            <meta property="og:video:width" content="2560">
+            <meta property="og:video:height" content="1440">
+            <meta property="og:image" content="https://example.com/rock.jpg">
+            <meta property="og:image:width" content="1280">
+            <meta property="og:image:height" content="720">
+        </head>
+        <body>
+            <h1>Sample HTML</h1>
+        </body>
+        </html>
+        """
+    }
+
+    var sampleVideoMissingMetadataHTML: String {
+        """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta property="og:type" content="video.other">
+            <meta property="og:video:width" content="1280">
+        </head>
+        <body>
+            <h1>Video with less metadata</h1>
+        </body>
+        </html>
+        """
+    }
+
+    var sampleWebsiteHTML: String {
+        """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta property="og:type" content="website">
+            <meta property="og:image:url" content="https://example.com/rock.jpg">
+            <meta property="og:image:width" content="640">
+            <meta property="og:image:height" content="480">
+        </head>
+        <body>
+            <blink>Welcome to my website!</blink>
+        </body>
+        </html>
+        """
+    }
+
+    var sampleWebsiteMissingMetadataHTML: String {
+        """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta property="og:image:width" content="640">
+        </head>
+        <body>
+            <blink>Welcome to my website!</blink>
+        </body>
+        </html>
+        """
     }
 }
--- NosTests/Service/Media/DefaultOpenGraphServiceTests.swift
+++ NosTests/Service/Media/DefaultOpenGraphServiceTests.swift
@@ -5,12 +5,11 @@ class DefaultOpenGraphServiceTests: XCTestCase {
         // Arrange
         let url = try XCTUnwrap(URL(string: "https://youtu.be/5qvdbyRH9wA?si=y_KTgLR22nH0-cs8"))
         let subject = DefaultOpenGraphService(session: MockURLSession(), parser: MockOpenGraphParser())
-        let expected = OpenGraphMetadata(media: nil)
 
         // Act
         let metadata = try await subject.fetchMetadata(for: url)
 
         // Assert
-        XCTAssertEqual(metadata, expected)
+        XCTAssertNil(metadata)
     }
 }
