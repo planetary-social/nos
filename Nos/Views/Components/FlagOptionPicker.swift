af6f782171e1beb137fe97e2a1bcb779bba71bb9
Itunu Raimi <chavitunu@gmail.com>
2024-09-19 14:25:45 +0100

    update FlagOptionPicker

--- Nos/Views/Components/FlagOptionPicker.swift
+++ Nos/Views/Components/FlagOptionPicker.swift
@@ -6,7 +6,12 @@ struct FlagOptionPicker: View {
     var title: String
     var subtitle: String?
 
-    init(selectedOption: Binding<FlagOption?>, options: [FlagOption], title: String, subtitle: String?) {
+    init(
+        selectedOption: Binding<FlagOption?>,
+        options: [FlagOption],
+        title: String,
+        subtitle: String?
+    ) {
         self._selectedOption = selectedOption
         self.options = options
         self.title = title
@@ -16,19 +21,20 @@ struct FlagOptionPicker: View {
     var body: some View {
         VStack(alignment: .leading) {
             HeaderView(text: title)
-            
             if let subtitle = subtitle {
                 HeaderView(text: subtitle)
             }
             flagOptionsListView
         }
-        .padding()
     }
 
     private var flagOptionsListView: some View {
         VStack(alignment: .leading) {
             ForEach(options) { flag in
-                FlagPickerRow(flag: flag, selection: $selectedOption)
+                FlagPickerRow(
+                    flag: flag,
+                    selection: $selectedOption
+                )
                 BeveledSeparator()
             }
         }
@@ -38,7 +44,7 @@ struct FlagOptionPicker: View {
 }
 
 /// A single row for a single flag option
-struct FlagPickerRow: View {
+private struct FlagPickerRow: View {
     var flag: FlagOption
     @Binding var selection: FlagOption?
 
@@ -47,29 +53,59 @@ struct FlagPickerRow: View {
     }
 
     var body: some View {
-        HStack(alignment: .center) {
-            VStack(alignment: .leading, spacing: 8) {
-                Text(flag.title)
-                    .foregroundColor(.primaryTxt)
-                    .font(.clarity(.regular))
-
-                if let description = flag.description {
-                    Text(description)
-                        .foregroundColor(.secondaryTxt)
-                        .font(.clarity(.regular, textStyle: .footnote))
-                        .lineSpacing(8)
-                }
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                flagContent
+                Spacer()
+                radioButton
             }
 
-            Spacer()
-
-            Button {
-                selection = flag
-            } label: {
-                NosRadioButton(isSelected: isSelected)
+            if let info = flag.info, selection?.id == flag.id {
+                infoBox(text: info)
             }
         }
         .padding(12)
+    }
+
+    private var radioButton: some View {
+        Button {
+            selection = flag
+        } label: {
+            NosRadioButton(isSelected: isSelected)
+        }
+    }
+
+    /// Displays the content of a flag option.
+    /// Shows the title of the flag option and its description (if available).
+    private var flagContent: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(flag.title)
+                .foregroundColor(.primaryTxt)
+                .font(.clarity(.regular))
+
+            if let description = flag.description {
+                Text(description)
+                    .foregroundColor(.secondaryTxt)
+                    .font(.clarity(.regular, textStyle: .footnote))
+                    .lineSpacing(8)
+                    .fixedSize(horizontal: false, vertical: true) // this enables the text view expand as needed
+            }
+        }
+    }
+
+    /// Displays additional information about a flag option.
+    private func infoBox(text: String) -> some View {
+        Text(text)
+            .foregroundColor(.primaryTxt)
+            .font(.clarity(.regular, textStyle: .subheadline))
+            .multilineTextAlignment(.leading)
+            .lineSpacing(8)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.purple.opacity(0.1))
+            )
+            .padding(.top, 12)
     }
 }
 
@@ -80,11 +116,11 @@ private struct HeaderView: View {
             .lineSpacing(5)
             .foregroundColor(.primaryTxt)
             .font(.clarity(.bold))
-            .padding(.bottom, 28)
+            .padding(.bottom, 10)
     }
 }
 
-#Preview {
+#Preview("Category Selection") {
     struct PreviewWrapper: View {
         @State private var selectedFlag: FlagOption?
 
@@ -97,6 +133,26 @@ private struct HeaderView: View {
             )
             .onAppear {
                 selectedFlag = FlagOption.flagContentCategories.first
+            }
+            .background(Color.appBg)
+        }
+    }
+    return PreviewWrapper()
+}
+
+#Preview("Send Selection") {
+    struct PreviewWrapper: View {
+        @State private var selectedFlag: FlagOption?
+
+        var body: some View {
+            FlagOptionPicker(
+                selectedOption: $selectedFlag,
+                options: FlagOption.flagContentSendCategories,
+                title: "Send to Nos or Flag Publicly",
+                subtitle: nil
+            )
+            .onAppear {
+                selectedFlag = FlagOption.flagContentSendCategories.first
             }
             .background(Color.appBg)
         }
