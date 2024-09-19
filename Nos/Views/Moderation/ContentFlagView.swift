3dfe81e3f72091cbe8fd6b1322b7dd4c5baee2c5
Itunu Raimi <chavitunu@gmail.com>
2024-09-19 14:26:16 +0100

    update ContentFlagView

--- Nos/Views/Moderation/ContentFlagView.swift
+++ Nos/Views/Moderation/ContentFlagView.swift
@@ -1,25 +1,51 @@
 import SwiftUI
 
-/// Displays pickers for selecting content flag option category, with additional stages shown 
+/// Displays pickers for selecting content flag option category, with additional stages shown
 /// based on previous selections.
 struct ContentFlagView: View {
     @Binding var selectedFlagOptionCategory: FlagOption?
+    @Binding var selectedSendOptionCategory: FlagOption?
 
     var body: some View {
         ScrollView {
-            VStack {
+            VStack(spacing: 30) {
                 FlagOptionPicker(
                     selectedOption: $selectedFlagOptionCategory,
                     options: FlagOption.flagContentCategories,
                     title: String(localized: .localizable.reportContent),
                     subtitle: String(localized: .localizable.reportContentMessage)
                 )
+
+                if selectedFlagOptionCategory != nil {
+                    FlagOptionPicker(
+                        selectedOption: $selectedSendOptionCategory,
+                        options: FlagOption.flagContentSendCategories,
+                        title: String(localized: .localizable.flagContentSendTitle),
+                        subtitle: nil
+                    )
+                }
             }
         }
+        .padding()
         .background(Color.appBg)
     }
 }
 
 #Preview {
-    ContentFlagView(selectedFlagOptionCategory: .constant(nil))
+    struct PreviewWrapper: View {
+        @State private var selectedFlagOptionCategory: FlagOption?
+        @State private var selectedSendOptionCategory: FlagOption?
+
+        var body: some View {
+            ContentFlagView(
+                selectedFlagOptionCategory: $selectedFlagOptionCategory,
+                selectedSendOptionCategory: $selectedSendOptionCategory
+            )
+            .onAppear {
+                selectedFlagOptionCategory = nil
+            }
+            .background(Color.appBg)
+        }
+    }
+    return PreviewWrapper()
 }
