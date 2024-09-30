29ced9f3e10c784578e71e40ca777a10a9b6031b
Itunu Raimi <chavitunu@gmail.com>
2024-09-30 16:37:25 +0100

    move FlagSuccessView to different flag

---
+++ Nos/Views/Moderation/FlagSuccessView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+/// Shows a success message after the user has successfully flagged a user or content
+struct FlagSuccessView: View {
+    var body: some View {
+        VStack(spacing: 30) {
+            Image.circularCheckmark
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 116)
+
+            Text(String(localized: .localizable.thanksForTag))
+                .foregroundColor(.primaryTxt)
+                .font(.clarity(.regular, textStyle: .title2))
+                .padding(.horizontal, 62)
+
+            Text(String(localized: .localizable.keepOnHelpingUs))
+                .padding(.horizontal, 68)
+                .foregroundColor(.secondaryTxt)
+                .multilineTextAlignment(.center)
+                .lineSpacing(6)
+                .font(.clarity(.regular, textStyle: .subheadline))
+        }
+    }
+}
+
+#Preview {
+    FlagSuccessView()
+}
