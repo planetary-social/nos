de4c0c4c074f70f611e470cdef87037ed07dd7a7
Itunu Raimi <chavitunu@gmail.com>
2024-10-17 19:05:45 +0100

    update Featured author list

--- Nos/Views/Discover/FeaturedAuthor.swift
+++ Nos/Views/Discover/FeaturedAuthor.swift
@@ -1,3 +1,4 @@
+// swiftlint:disable file_length
 import Foundation
 
 /// An author that's featured on the Discover tab.
@@ -31,7 +32,7 @@ struct FeaturedAuthor {
 
 extension FeaturedAuthor {
     /// All featured authors that should appear on the Discover tab.
-    static let all = cohort4 + cohort3 + cohort2 + cohort1 + additionalAuthors
+    static let all = cohort5 + cohort4 + cohort3 + cohort2 + cohort1 + additionalAuthors
 }
 
 extension FeaturedAuthor {
@@ -83,11 +84,6 @@ extension FeaturedAuthor {
             categories: [.music]
         ),
         FeaturedAuthor(
-            name: "Onigirl",
-            npub: "npub18jvyjwpmm65g8v9azmlvu8knd5m7xlxau08y8vt75n53jtkpz2ys6mqqu3",
-            categories: [.art]
-        ),
-        FeaturedAuthor(
             name: "Black in Aquatics",
             npub: "npub1ee852umhw26afdfwgf90cky9ufaythwyarttqu59lazcrx56z0tsmx3mkz",
             categories: [.sports]
@@ -96,12 +92,7 @@ extension FeaturedAuthor {
             name: "Global Sports Central",
             npub: "npub1qspus6smkn8mcdxg5jflh50s69vdgtwsd5p74gmjpzp2qekn5duqfv5afj",
             categories: [.news, .sports]
-        ),
-        FeaturedAuthor(
-            name: "Alastair Thompson",
-            npub: "npub157pk8t8njtnldqzankrk2syzmkp6qtrv2ewgq3fnuc4k78dr797shfngev",
-            categories: [.news]
-        ),
+        )
     ]
 }
 
@@ -188,24 +179,9 @@ extension FeaturedAuthor {
 extension FeaturedAuthor {
     static let cohort4 = [
         FeaturedAuthor(
-            name: "NDP Media",
-            npub: "npub1ks7de0smt2v3aytrdsfh927sgml3666459mj9ghd7tvg3t42x9gqyvwjqf",
-            categories: [.news]
-        ),
-        FeaturedAuthor(
-            name: "Revolution Z",
-            npub: "npub1j4sumq8pyplks5nhchyhzmw72dyemkyv2fv50vwau5fhf2qa7zusq3tlyh",
-            categories: [.news, .activists]
-        ),
-        FeaturedAuthor(
             name: "Existing Sprinkles",
             npub: "npub1f5kc2agn63ecv2ua4909z9ahgmr2x9263na36jh6r908ql0926jq3nvk2u",
             categories: [.art]
-        ),
-        FeaturedAuthor(
-            name: "MuckRock",
-            npub: "npub1u5nfvj4dzzmrcf9n5kptl26w7kfhc4vml0ll8svvhrv5jz2es46sl0vv2r",
-            categories: [.news]
         ),
         FeaturedAuthor(
             name: "ArchJourney",
@@ -227,5 +203,345 @@ extension FeaturedAuthor {
             npub: "npub1z3thwmwasmp787zvk2aaq5qdjtjdkl637p52nph4flv668973c8qaz2du7",
             categories: [.activists]
         ),
+    ]
+}
+
+extension FeaturedAuthor {
+    static let cohort5 = [
+        FeaturedAuthor(
+            name: "kindness project bot ",
+            npub: "npub1r8yz465alvfnykhkfula8kxxeas2z75hats0tymw8yv3kp0p4a5qp6phl8",
+            categories: [.health]
+        ),
+        FeaturedAuthor(
+            name: "Pegah",
+            npub: "npub1rt65j4vnd05qf72szpj8afdd5hrkylka7pe55lup8waquv0wm9sqjj0y9g",
+            categories: [.art]
+        ),
+        FeaturedAuthor(
+            name: "Karine Studio",
+            npub: "npub1l9kr6ajfwp6vfjp60vuzxwqwwlw884dff98a9cznujs520shsf9s35xfwh",
+            categories: [.art]
+        ),
+        FeaturedAuthor(
+            name: "Kavinya",
+            npub: "npub1w3myuqcf6gzgluqtn0mzpmqyqyqlzhtrhdsna8t287mggjg9j6zs2xjh9z",
+            categories: [.music]
+        ),
+        FeaturedAuthor(
+            name: "NOICE",
+            npub: "npub1v02emwxjn2lznkmnszltjy4c7cqzxuejkmye0zeqs62wf0shpahswdmwuj",
+            categories: [.art, .photography]
+        ),
+        FeaturedAuthor(
+            name: "Matt Birchler",
+            npub: "npub13huwfylt3hm77wxk9js63k0y3p03u3xhj39z49hnkdcydvp0ph6sr573xv",
+            categories: [.tech]
+        ),
+        FeaturedAuthor(
+            name: "John Gruber",
+            npub: "npub19tmpp2jx64y4ggf9d7gyjdvtrjckx4udjal4sc77s62sfm3cfslqmkc8af",
+            categories: [.tech]
+        ),
+        FeaturedAuthor(
+            name: "Meredith Whittaker",
+            npub: "npub15s3hussvmv9nyvw3w8lg0x72udazmdat7tcj5vmmjafnwcvv8tpqzz6r6y",
+            categories: [.tech]
+        ),
+        FeaturedAuthor(
+            name: "Charles Stross",
+            npub: "npub1yacj26q9parem5fqf2s3ym9xkxghkk0w2jc63aglsnl2fmyhshnszgla7e",
+            categories: [.scifi]
+        ),
+        FeaturedAuthor(
+            name: "Pablonius Monk",
+            npub: "npub1cxl7dfnmy669w65uyc27f0kwwg0p8dgfz9h2w36jpt7q90v99wcqpluwyu",
+            categories: [.politics]
+        ),
+        FeaturedAuthor(
+            name: "Taylor Lorenz",
+            npub: "npub1d9nndmy3lx6f00cysrmn2v9t6hz280uwycw0kgcfdhvg99azry8sududfv",
+            categories: [.tech]
+        ),
+        FeaturedAuthor(
+            name: "zed-erwan",
+            npub: "npub1r2sah0htqnw7xrs70gq00m48vp25neu8ym2n2ghrny92dqqf7sest8hth0",
+            categories: [.art]
+        ),
+        FeaturedAuthor(
+            name: "Franny",
+            npub: "npub1995y964wmxl94crx3ksfley24szjr390skdd237ex9z7ttp5c9lqld8vtf",
+            categories: [.photography]
+        ),
+        FeaturedAuthor(
+            name: "Johanna",
+            npub: "npub1a2a85jwde32zjsjk02ujasydqc3t2w9rfgfe97amm0r4d9mepfxsxf3fnn",
+            categories: [.health]
+        ),
+        FeaturedAuthor(
+            name: "Alisa",
+            npub: "npub1q4e23x480fcn2wpxh7ufl8h2mtd06g0ynjdeaqjgqtkn2pae2awsd95ftj",
+            categories: [.food]
+        ),
+        FeaturedAuthor(
+            name: "ProPublica",
+            npub: "npub19fwwstv5dg8qsmuj9rmgf98nt9lfz5gvv67jqx6y9jtgekpcz5pqpgulzp",
+            categories: [.news]
+        ),
+        FeaturedAuthor(
+            name: "Robert Reich",
+            npub: "npub1dzkq7f7q23fh0mrw03wwd23ddmu258k6m34gctl6ark6qlex4l7qskqcce",
+            categories: [.news]
+        ),
+        FeaturedAuthor(
+            name: "Internet Archive",
+            npub: "npub13f3xuy2npzmpctnv3z4c0rte3ntsl7kflwcrkt97zrltlq68d5ws0pfd3f",
+            categories: [.news]
+        ),
+        FeaturedAuthor(
+            name: "Electronic Frontier Foundation",
+            npub: "npub1q40jtk09x6mdmmlw09qracctqpgsr9zl2hcc7wzf8vuwasqsncwsxgaa27",
+            categories: [.news]
+        ),
+        FeaturedAuthor(
+            name: "Eliza",
+            npub: "npub1yye4qu6qrgcsejghnl36wl5kvecsel0kxr0ass8ewtqc8gjykxkssdhmd0",
+            categories: [.art]
+        ),
+        FeaturedAuthor(
+            name: "MLS Major League Soccer",
+            npub: "npub1tql0248ettpuxngcmyqqt7q83zacx4ajxg3kv35ptayzd3nw2jnqxnwxsu",
+            categories: [.sports]
+        ),
+        FeaturedAuthor(
+            name: "Ava",
+            npub: "npub1f6ugxyxkknket3kkdgu4k0fu74vmshawermkj8d06sz6jts9t4kslazcka",
+            categories: [.tech]
+        ),
+        FeaturedAuthor(
+            name: "Eva",
+            npub: "npub18wn0jd3p7n6u3y7mc46p0hpx3cmtv9k3mu82rc0lhkg3rdyf590s3wshpx",
+            categories: [.tech, .activists]
+        ),
+        FeaturedAuthor(
+            name: "Strypey",
+            npub: "npub1pwwm8s9zxssfm3aqv3g4fvqfh9kglr76m5z434cymju9gp9jhwwqdqtc65",
+            categories: [.tech, .activists]
+        ),
+        FeaturedAuthor(
+            name: "Cecilia",
+            npub: "npub1paftqx5zvj0gjtu3whudejxdgv4vdn27paw4lzaauc43kgfxxaqs75c5a7",
+            categories: [.art]
+        ),
+        FeaturedAuthor(
+            name: "Alvin",
+            npub: "npub1ansj0chcjdksv5m83lkshkyah5jxky6uqupgtphemsr27azm4wwqz0epd5",
+            categories: [.tech, .espanol]
+        ),
+        FeaturedAuthor(
+            name: "Zoé",
+            npub: "npub16wy27uj48r82gskq48uvxku8076h0y9xcngsgry7j4yn6zxmnznqu4hy6a",
+            categories: [.photography, .espanol]
+        ),
+        FeaturedAuthor(
+            name: "Melvin Carvalho",
+            npub: "npub1melv683fw6n2mvhl5h6dhqd8mqfv3wmxnz4qph83ua4dk4006ezsrt5c24",
+            categories: [.tech]
+        ),
+        FeaturedAuthor(
+            name: "Just Loud",
+            npub: "npub1a377f258tzafuzdgezwjw4aplg8ze3suyzm40t6j4czcjvwp5vls5d5r4w",
+            categories: [.music]
+        ),
+        FeaturedAuthor(
+            name: "Paloma",
+            npub: "npub1gscnk7wlcvcruw7scjhwpjrja94gfu369fzkyje6kcc0yn6rqyhscqtpjp",
+            categories: [.lifestyle]
+        ),
+        FeaturedAuthor(
+            name: "Cory doctorow",
+            npub: "npub1yxzkmtuyctjw2pffp6e9uvyrkp29hrqra2tm3xp3z9707z06muxsg75qvv",
+            categories: [.tech, .activists]
+        ),
+        FeaturedAuthor(
+            name: "Sitko",
+            npub: "npub12rze589jx0gg6kslkjfl2gxxkhtlw73t5shyve5qrglrv6c2qflqejj7ns",
+            categories: [.photography]
+        ),
+        FeaturedAuthor(
+            name: "Ratel",
+            npub: "npub15c3sa5kzedkfys95mc5egmst8ptuqefvezemae0883un7hqf5d5sgmqlxf",
+            categories: [.art]
+        ),
+        FeaturedAuthor(
+            name: "Wellington Chinese History",
+            npub: "npub1fdyax3st9gcqj7zhqres3fw9pf7qrkukppv3fs7w2mcjcsqyrxjqcgcxcn",
+            categories: [.newzealand]
+        ),
+        FeaturedAuthor(
+            name: "David Hood",
+            npub: "npub12aew64q5lsxpcc98lskha0564gtjn30cw7vdueqnyj06xjqxtmksahdrlg",
+            categories: [.newzealand, .news]
+        ),
+        FeaturedAuthor(
+            name: "Kris Sowersby",
+            npub: "npub1t6n8u5sa80m6sx53m7uge6afktek4me982wvclr3xe5tfeyg34fslkxqcz",
+            categories: [.art, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Greenpeace Atoearoa",
+            npub: "npub1mn78z9rssfq2yjqdwsa3h4qwu6zpw4xku9ppfldkde4ycwnpch4qhmcssl",
+            categories: [.activists, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Bill Bennett",
+            npub: "npub1gpcxn339ap3r9tju2uy6d5k8rmud7f8kr579w7zv54qyevgz9xsqdtr4us",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "The Spinoff",
+            npub: "npub1qngl40pxy06k3hrqp4ltkn4p5yacen7u93du58v4tamf73tzaqhs6dfy8g",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Dan Selvin",
+            npub: "npub16jjukmhnvflj92dv2jr8z6udfhzyyuyf3mckmf6afws9w4xdhhzs745kmv",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Emma Cook",
+            npub: "npub18pl5kgxl47e5pssxl9q38m2v7ez477gw3nz220jz2vslnjreqq8s2yxl9m",
+            categories: [.art, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Jack Yan",
+            npub: "npub179lhhjz30t3n4utkkn26qzgmpx44v9s5qdtwjs5686px8ulw7dfszxmfgp",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Lucire",
+            npub: "npub1l4s4mtt96znwu3plfeyupk37y63xfapw5e7kjn7uavuw02lvav5q3ngc3s",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Ian Griffin",
+            npub: "npub19p6edn6lvz9dmegm7s8vyz9w5w0ejzgztke8rcswxgmqr35h8feszt36jc",
+            categories: [.scifi, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "JSM",
+            npub: "npub1uqeexjx2djkfwzxdnrnrrch5h2k4xn0uapcgsxm94ftaxrlhy5lqywjckg",
+            categories: [.tech]
+        ),
+        FeaturedAuthor(
+            name: "Carla",
+            npub: "npub1hu3hdctm5nkzd8gslnyedfr5ddz3z547jqcl5j88g4fame2jd08qh6h8nh",
+            categories: [.random]
+        ),
+        FeaturedAuthor(
+            name: "WaveLake",
+            npub: "npub1yfg0d955c2jrj2080ew7pa4xrtj7x7s7umt28wh0zurwmxgpyj9shwv6vg",
+            categories: [.music]
+        ),
+        FeaturedAuthor(
+            name: "Angela",
+            npub: "npub19vvkfwy9mcluhvehw7r56p4stsj5lmx4v9g3vgkwsm3arpgef8aqsrt562",
+            categories: [.random]
+        ),
+        FeaturedAuthor(
+            name: "J",
+            npub: "npub1fv9u4drq4hdrr7k45vn0krqy7mkgy8ajf059m0wq8szvcrsjlsrs8tdz3p",
+            categories: [.food, .random]
+        ),
+        FeaturedAuthor(
+            name: "Connie",
+            npub: "npub1468024mrwz6uhywjvt8s6vw4e604egnv8tfd2m2utrmqyd3nu3fsutvgjn",
+            categories: [.random]
+        ),
+        FeaturedAuthor(
+            name: "Ignacio",
+            npub: "npub1wptpq88jyjv6nwwltqx0exhxsg8zt0dcm4uwvtcxn80hxk2r66hswdm8rf",
+            categories: [.lifestyle, .espanol]
+        ),
+        FeaturedAuthor(
+            name: "Nacho",
+            npub: "npub1daajnadf0f0s7uz3yftur8434rtz2s949gkdpx7uyeapm9rlt0qq9q8w5z",
+            categories: [.lifestyle]
+        )
+    ]
+}
+
+extension FeaturedAuthor {
+    static let selectNew = [
+        FeaturedAuthor(
+            name: "NOICE",
+            npub: "npub1v02emwxjn2lznkmnszltjy4c7cqzxuejkmye0zeqs62wf0shpahswdmwuj",
+            categories: [.art, .photography]
+        ),
+        FeaturedAuthor(
+            name: "Wellington Chinese History",
+            npub: "npub1fdyax3st9gcqj7zhqres3fw9pf7qrkukppv3fs7w2mcjcsqyrxjqcgcxcn",
+            categories: [.newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Ignacio",
+            npub: "npub1wptpq88jyjv6nwwltqx0exhxsg8zt0dcm4uwvtcxn80hxk2r66hswdm8rf",
+            categories: [.lifestyle, .espanol]
+        ),
+        FeaturedAuthor(
+            name: "Greenpeace Atoearoa",
+            npub: "npub1mn78z9rssfq2yjqdwsa3h4qwu6zpw4xku9ppfldkde4ycwnpch4qhmcssl",
+            categories: [.activists, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Bill Bennett",
+            npub: "npub1gpcxn339ap3r9tju2uy6d5k8rmud7f8kr579w7zv54qyevgz9xsqdtr4us",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "The Spinoff",
+            npub: "npub1qngl40pxy06k3hrqp4ltkn4p5yacen7u93du58v4tamf73tzaqhs6dfy8g",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Dan Selvin",
+            npub: "npub16jjukmhnvflj92dv2jr8z6udfhzyyuyf3mckmf6afws9w4xdhhzs745kmv",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Nacho",
+            npub: "npub1daajnadf0f0s7uz3yftur8434rtz2s949gkdpx7uyeapm9rlt0qq9q8w5z",
+            categories: [.lifestyle]
+        ),
+        FeaturedAuthor(
+            name: "Emma Cook",
+            npub: "npub18pl5kgxl47e5pssxl9q38m2v7ez477gw3nz220jz2vslnjreqq8s2yxl9m",
+            categories: [.art, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Jack Yan",
+            npub: "npub179lhhjz30t3n4utkkn26qzgmpx44v9s5qdtwjs5686px8ulw7dfszxmfgp",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Lucire",
+            npub: "npub1l4s4mtt96znwu3plfeyupk37y63xfapw5e7kjn7uavuw02lvav5q3ngc3s",
+            categories: [.news, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "Ian Griffin",
+            npub: "npub19p6edn6lvz9dmegm7s8vyz9w5w0ejzgztke8rcswxgmqr35h8feszt36jc",
+            categories: [.scifi, .newzealand]
+        ),
+        FeaturedAuthor(
+            name: "David Hood",
+            npub: "npub12aew64q5lsxpcc98lskha0564gtjn30cw7vdueqnyj06xjqxtmksahdrlg",
+            categories: [.newzealand, .news]
+        ),
+        FeaturedAuthor(
+            name: "Kris Sowersby",
+            npub: "npub1t6n8u5sa80m6sx53m7uge6afktek4me982wvclr3xe5tfeyg34fslkxqcz",
+            categories: [.art, .newzealand]
+        )
     ]
 }
