cb7409d665059f2fb4a9dde4f78e7e1a683f4e02
Itunu Raimi <chavitunu@gmail.com>
2024-10-18 14:58:06 +0100

    split featured authors cohort into different files.

---
+++ Nos/Views/Discover/FeaturedAuthor+Cohort1.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+extension FeaturedAuthor {
+    static let cohort1 = [
+        FeaturedAuthor(
+            name: "Miguel Almodo",
+            npub: "npub1ajt9gp0prf4xrp4j07j9rghlcyukahncs0fw5ywr977jccued9nqrcc0cs",
+            categories: [.activists]
+        ),
+        FeaturedAuthor(
+            name: "The Conversation",
+            npub: "npub1uuxnz0sq60thc098xfxqst7wnw77l0sm3r8nn48yspuvz4ecprksxdahzv",
+            categories: [.news]
+        ),
+        FeaturedAuthor(
+            name: "Ainsley Costello",
+            npub: "npub13qrrw2h4z52m7jh0spefrwtysl4psfkfv6j4j672se5hkhvtyw7qu0almy",
+            categories: [.music]
+        ),
+        FeaturedAuthor(
+            name: "Chris Liss",
+            npub: "npub1dwhr8j9uy6ju2uu39t6tj6mw76gztr4rwdd6jr9qtkdh5crjwt5q2nqfxe",
+            categories: [.sports]
+        ),
+        FeaturedAuthor(
+            name: "Alicia Stockman",
+            npub: "npub1l3y60kjywvrrln5ftse553h2ltg53sm3zy55grvlncd78x3k5uqsmw8dff",
+            categories: [.music]
+        ),
+        FeaturedAuthor(
+            name: "Judson McKinney and the Wanderers",
+            npub: "npub1jvt2hacqqzvwjkum30mlvmy52jer4p4crfh0veqstpk58rr9e7ms2fwh74",
+            categories: [.music]
+        ),
+        FeaturedAuthor(
+            name: "Black in Aquatics",
+            npub: "npub1ee852umhw26afdfwgf90cky9ufaythwyarttqu59lazcrx56z0tsmx3mkz",
+            categories: [.sports]
+        ),
+        FeaturedAuthor(
+            name: "Global Sports Central",
+            npub: "npub1qspus6smkn8mcdxg5jflh50s69vdgtwsd5p74gmjpzp2qekn5duqfv5afj",
+            categories: [.news, .sports]
+        )
+    ]
+}
---
+++ Nos/Views/Discover/FeaturedAuthor+Cohort4.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+extension FeaturedAuthor {
+    static let cohort4 = [
+        FeaturedAuthor(
+            name: "Existing Sprinkles",
+            npub: "npub1f5kc2agn63ecv2ua4909z9ahgmr2x9263na36jh6r908ql0926jq3nvk2u",
+            categories: [.art]
+        ),
+        FeaturedAuthor(
+            name: "ArchJourney",
+            npub: "npub1qhjxfxpjm7udr0agr6nuhuwf9383e4g9907g64r9hf6y4fh6t6uqpcp36k",
+            categories: [.art]
+        ),
+        FeaturedAuthor(
+            name: "Neigsendoig Cocules",
+            npub: "npub1372csjhjv35sxcqm90ca2d0cfxsl6xku7j6hhswynwdy9m7zl98scn950w",
+            categories: [.music]
+        ),
+        FeaturedAuthor(
+            name: "Lexie Bean",
+            npub: "npub1s8c5mk68qn0erxrx5waqz7xxk39x5xx2367879eqcv270tqs4tvsf5ewgf",
+            categories: [.activists]
+        ),
+        FeaturedAuthor(
+            name: "Protest.net",
+            npub: "npub1z3thwmwasmp787zvk2aaq5qdjtjdkl637p52nph4flv668973c8qaz2du7",
+            categories: [.activists]
+        )
+    ]
+}
