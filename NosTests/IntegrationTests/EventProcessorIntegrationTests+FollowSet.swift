39bef4c528cfbbe51cf5681fdca8b6b65770a96e
Bryan Montz <bryanmontz@me.com>
2024-12-29 08:57:20 -0600

    added support for decrypting private tags in kind 30000 lists

--- NosTests/IntegrationTests/EventProcessorIntegrationTests+FollowSet.swift
+++ NosTests/IntegrationTests/EventProcessorIntegrationTests+FollowSet.swift
@@ -73,4 +73,41 @@ extension EventProcessorIntegrationTests {
         let authorPubKey = try XCTUnwrap(followSet.authors.first?.hexadecimalPublicKey)
         XCTAssertEqual(authorPubKey, "27cf2c68535ae1fc06510e827670053f5dcd39e6bd7e05f1ffb487ef2ac13549")
     }
+    
+    @MainActor func test_parse_kind_30000_with_private_tags() throws {
+        // Arrange
+        let replaceableID = "PrivateOnly"
+        let ownerPubKey = "ffd99f9e545b53e3291dab4b8cd6d25d12b9973c40f02e1938c0891d62e38e57"
+        let data = try jsonData(filename: "follow_set_private")
+        let jsonEvent = try JSONDecoder().decode(JSONEvent.self, from: data)
+
+        // Act
+        _ = try EventProcessor.parse(
+            jsonEvent: jsonEvent,
+            from: nil,
+            in: testContext,
+            skipVerification: true,
+            keyPair: KeyPair(nsec: "nsec17vdaesh5tp6u5dy74vdy7a7e5x5ww4wfdnrn6ewgnsfxav8pcurqnlmj88")
+        )
+
+        // Assert
+        let fetchResults = try testContext.fetch(
+            AuthorList.authorList(
+                by: replaceableID,
+                owner: try Author.findOrCreate(by: ownerPubKey, context: testContext),
+                kind: EventKind.followSet.rawValue
+            )
+        )
+        XCTAssertEqual(fetchResults.count, 1)
+
+        let followSet = try XCTUnwrap(fetchResults.first)
+        XCTAssertTrue(followSet.authors.isEmpty)
+        
+        XCTAssertEqual(followSet.privateAuthors.count, 3)
+        
+        let publicKeys = followSet.privateAuthors.map { $0.hexadecimalPublicKey }
+        XCTAssertTrue(publicKeys.contains("3efdaebb1d8923ebd99c9e7ace3b4194ab45512e2be79c1b7d68d9243e0d2681"))
+        XCTAssertTrue(publicKeys.contains("3743244390be53473a7e3b3b8d04dce83f6c9514b81a997fb3b123c072ef9f78"))
+        XCTAssertTrue(publicKeys.contains("fa984bd7dbb282f07e16e7ae87b26a2a7b9b90b7246a44771f0cf5ae58018f52"))
+    }
 }
